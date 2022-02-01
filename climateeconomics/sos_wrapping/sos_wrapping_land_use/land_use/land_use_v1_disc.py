b2f0813c5c38d47cf9457cba2909bc804b6fab95
Julien Souchard <NG92D02@TOLX302085985.tls.fr.eu.airbus.corp>
2022-02-01 16:40:59 +0100

    Deepcopy land_surface_detailed_df to avoid mismatch after compute_sos_jacobian

--- climateeconomics/sos_wrapping/sos_wrapping_land_use/land_use/land_use_v1_disc.py
+++ climateeconomics/sos_wrapping/sos_wrapping_land_use/land_use/land_use_v1_disc.py
@@ -24,6 +24,8 @@ from sos_trades_core.tools.post_processing.plotly_native_charts.instantiated_plo
 
 import os
 import pandas as pd
+from copy import deepcopy
+
 
 class LandUseV1Discipline(SoSDiscipline):
     ''' Discipline intended to host land use model with land use for food input from agriculture model
@@ -34,9 +36,10 @@ class LandUseV1Discipline(SoSDiscipline):
     DESC_IN = {'year_start': {'type': 'int', 'default': default_year_start, 'unit': '[-]', 'visibility': SoSDiscipline.SHARED_VISIBILITY, 'namespace': 'ns_public'},
                'year_end': {'type': 'int', 'default': default_year_end, 'unit': '[-]', 'visibility': SoSDiscipline.SHARED_VISIBILITY, 'namespace': 'ns_public'},
                LandUseV1.LAND_DEMAND_DF: {'type': 'dataframe', 'unit': 'Gha',
-                                        'visibility': SoSDiscipline.SHARED_VISIBILITY, 'namespace': 'ns_land_use'},
+                                          'visibility': SoSDiscipline.SHARED_VISIBILITY, 'namespace': 'ns_land_use'},
                LandUseV1.TOTAL_FOOD_LAND_SURFACE: {'type': 'dataframe', 'unit': 'Gha', 'visibility': SoSDiscipline.SHARED_VISIBILITY, 'namespace': 'ns_witness'},
-               LandUseV1.DEFORESTED_SURFACE_DF: {'type': 'dataframe', 'unit': 'Gha', 'visibility': SoSDiscipline.SHARED_VISIBILITY, 'namespace': 'ns_witness'}
+               LandUseV1.DEFORESTED_SURFACE_DF: {
+                   'type': 'dataframe', 'unit': 'Gha', 'visibility': SoSDiscipline.SHARED_VISIBILITY, 'namespace': 'ns_witness'}
                }
 
     DESC_OUT = {
@@ -72,11 +75,12 @@ class LandUseV1Discipline(SoSDiscipline):
         total_food_land_surface = inputs_dict.pop('total_food_land_surface')
         deforested_surface_df = inputs_dict.pop('deforested_surface_df')
         deforested_surface_df.index = land_demand_df['years']
-        self.land_use_model.compute(land_demand_df, total_food_land_surface, deforested_surface_df)
+        self.land_use_model.compute(
+            land_demand_df, total_food_land_surface, deforested_surface_df)
 
         outputs_dict = {
             LandUseV1.LAND_DEMAND_CONSTRAINT_DF: self.land_use_model.land_demand_constraint_df,
-            LandUseV1.LAND_SURFACE_DETAIL_DF: self.land_use_model.land_surface_df,
+            LandUseV1.LAND_SURFACE_DETAIL_DF: deepcopy(self.land_use_model.land_surface_df),
             LandUseV1.LAND_SURFACE_DF: self.land_use_model.land_surface_df[[
                 'Agriculture (Gha)', 'Forest (Gha)']],
             LandUseV1.LAND_SURFACE_FOR_FOOD_DF: self.land_use_model.land_surface_for_food_df
@@ -96,7 +100,8 @@ class LandUseV1Discipline(SoSDiscipline):
         total_food_land_surface = inputs_dict.pop('total_food_land_surface')
         deforested_surface_df = inputs_dict.pop('deforested_surface_df')
         model = self.land_use_model
-        model.compute(land_demand_df, total_food_land_surface, deforested_surface_df)
+        model.compute(land_demand_df, total_food_land_surface,
+                      deforested_surface_df)
 
         # Retrieve variables
         land_demand_df = model.land_demand_df
@@ -121,7 +126,7 @@ class LandUseV1Discipline(SoSDiscipline):
             for demand_column in land_demand_df_columns:
                 self.set_partial_derivative_for_other_types(
                     (LandUseV1.LAND_DEMAND_CONSTRAINT_DF, objective_column),  (LandUseV1.LAND_DEMAND_DF, demand_column), model.get_derivative(objective_column, demand_column),)
-               
+
             self.set_partial_derivative_for_other_types(
                 (LandUseV1.LAND_DEMAND_CONSTRAINT_DF, objective_column),  (LandUseV1.TOTAL_FOOD_LAND_SURFACE, 'total surface (Gha)'), model.d_land_demand_constraint_d_food_land_surface(objective_column),)
 
@@ -175,87 +180,94 @@ class LandUseV1Discipline(SoSDiscipline):
         years = demand_df['years'].values.tolist()
 
         if LandUseV1Discipline.FOREST_CHARTS in chart_list:
-            ## ------------------------------------------------------------
-            ## FOREST USAGE -> Technologies that uses forest (ManagedWood, UnmanagedWood..)
+            # ------------------------------------------------------------
+            # FOREST USAGE -> Technologies that uses forest (ManagedWood,
+            # UnmanagedWood..)
             if demand_df is not None:
                 forest_surfaces = surface_df['Forest (Gha)'].values
                 forest_surface_series = InstanciatedSeries(
                     years, forest_surfaces.tolist(), 'Available surface', InstanciatedSeries.LINES_DISPLAY)
-    
+
                 series_to_add = []
                 for column in list(demand_df):
-    
+
                     if column in LandUseV1.FOREST_TECHNO:
-    
+
                         new_series = InstanciatedSeries(
                             years, (demand_df[column]).values.tolist(), column.replace('(Gha)', ''), InstanciatedSeries.BAR_DISPLAY)
-    
+
                         series_to_add.append(new_series)
-    
+
                 new_chart = TwoAxesInstanciatedChart('years', LandUseV1Discipline.FOREST_CHARTS,
                                                      chart_name=LandUseV1Discipline.FOREST_CHARTS, stacked_bar=True)
                 new_chart.add_series(forest_surface_series)
-    
+
                 for serie in series_to_add:
                     new_chart.add_series(serie)
-    
+
                 instanciated_charts.append(new_chart)
 
         if LandUseV1Discipline.AGRICULTURE_CHARTS in chart_list:
-            ## ------------------------------------------------------------
-            ## AGRICULTURE USAGE -> Technologies that uses agriculture land (CropEnergy, SolarPV..)
+            # ------------------------------------------------------------
+            # AGRICULTURE USAGE -> Technologies that uses agriculture land
+            # (CropEnergy, SolarPV..)
             if demand_df is not None:
                 agriculture_surfaces = surface_df['Agriculture total (Gha)'].values
                 agriculture_surface_series = InstanciatedSeries(
                     years, agriculture_surfaces.tolist(), 'Total agriculture surface', InstanciatedSeries.LINES_DISPLAY)
-    
+
                 series_to_add = []
                 key = 'Food Usage (Gha)'
                 legend = 'Food usage : Crop & lifestock'
                 new_series = InstanciatedSeries(
-                        years, (surface_df[key]).values.tolist(), legend, InstanciatedSeries.BAR_DISPLAY)
-    
+                    years, (surface_df[key]).values.tolist(), legend, InstanciatedSeries.BAR_DISPLAY)
+
                 series_to_add.append(new_series)
-    
+
                 for column in list(demand_df):
-    
+
                     if column in LandUseV1.AGRICULTURE_TECHNO:
-    
+
                         new_series = InstanciatedSeries(
                             years, (demand_df[column]).values.tolist(), column.replace('(Gha)', ''), InstanciatedSeries.BAR_DISPLAY)
-    
+
                         series_to_add.append(new_series)
-    
+
                 new_chart = TwoAxesInstanciatedChart('years', LandUseV1Discipline.AGRICULTURE_CHARTS,
                                                      chart_name=LandUseV1Discipline.AGRICULTURE_CHARTS, stacked_bar=True)
                 new_chart.add_series(agriculture_surface_series)
-    
+
                 for serie in series_to_add:
                     new_chart.add_series(serie)
-    
+
                 instanciated_charts.append(new_chart)
 
         if LandUseV1Discipline.GLOBAL_CHARTS in chart_list:
-            ## ------------------------------------------------------------
-            ## GLOBAL LAND USE -> Display surfaces (Ocean, Land, Forest..)
+            # ------------------------------------------------------------
+            # GLOBAL LAND USE -> Display surfaces (Ocean, Land, Forest..)
             years_list = [self.get_sosdisc_inputs('year_start')]
-            ## ------------------
-            ## Sunburst figure for global land use. Source https://ourworldindata.org/land-use
+            # ------------------
+            # Sunburst figure for global land use. Source
+            # https://ourworldindata.org/land-use
             for year in years_list:
                 # Create figure
                 fig = go.Figure(go.Sunburst(
-                        labels=["Land", "Ocean", "Habitable land", "Glaciers", "Barren land", "Agriculture", "Forest", "Shrub", "Urban", "Fresh water"],
-                        parents=["Earth", "Earth", "Land", "Land", "Land", "Habitable land", "Habitable land", "Habitable land", "Habitable land", "Habitable land"],
-                        values=[14.9, 36.1, 10.5, 1.5, 2.8, 5.1, 3.9, 1.2, 0.15, 0.15],
-                        marker=dict(colors = ["#CD912A", "#1456C5", "#DBBF6A", "#D3D3D0", "#E7C841", "#7CC873", "#1EA02F", "#5C8C56", "#B1B4AF", "#18CDFA"]),
-                        branchvalues="total",
-                        rotation=90,
-                    )
+                    labels=["Land", "Ocean", "Habitable land", "Glaciers", "Barren land",
+                            "Agriculture", "Forest", "Shrub", "Urban", "Fresh water"],
+                    parents=["Earth", "Earth", "Land", "Land", "Land", "Habitable land",
+                             "Habitable land", "Habitable land", "Habitable land", "Habitable land"],
+                    values=[14.9, 36.1, 10.5, 1.5, 2.8,
+                            5.1, 3.9, 1.2, 0.15, 0.15],
+                    marker=dict(colors=["#CD912A", "#1456C5", "#DBBF6A", "#D3D3D0",
+                                        "#E7C841", "#7CC873", "#1EA02F", "#5C8C56", "#B1B4AF", "#18CDFA"]),
+                    branchvalues="total",
+                    rotation=90,
+                )
                 )
                 fig.update_layout(
-                autosize=True,
-                margin=dict(t=80, l=0, r=0, b=0)
-            )
+                    autosize=True,
+                    margin=dict(t=80, l=0, r=0, b=0)
+                )
 
                 # Create native plotly chart
                 chart_name = f'Global land use (Gha) in {year}'
@@ -265,8 +277,9 @@ class LandUseV1Discipline(SoSDiscipline):
                 instanciated_charts.append(land_use_IEA_chart)
 
             series_to_add = []
-            ## ------------------
-            ## Agriculture + forest surface are 92.03 M km^2 => 9.203 Gha. Source https://ourworldindata.org/land-use
+            # ------------------
+            # Agriculture + forest surface are 92.03 M km^2 => 9.203 Gha.
+            # Source https://ourworldindata.org/land-use
             earth_surface = [9.203] * len(years)
             forest_surface_series = InstanciatedSeries(
                 years, earth_surface, 'Available surface', InstanciatedSeries.LINES_DISPLAY)
@@ -274,7 +287,7 @@ class LandUseV1Discipline(SoSDiscipline):
             series_to_add.append(forest_surface_series)
 
             agriculture_surface = InstanciatedSeries(
-                years, (surface_df['Agriculture total (Gha)']).values.tolist(),surface_df['Agriculture total (Gha)'].name.replace(' total (Gha)', ''), InstanciatedSeries.BAR_DISPLAY)
+                years, (surface_df['Agriculture total (Gha)']).values.tolist(), surface_df['Agriculture total (Gha)'].name.replace(' total (Gha)', ''), InstanciatedSeries.BAR_DISPLAY)
 
             series_to_add.append(agriculture_surface)
 
@@ -284,8 +297,8 @@ class LandUseV1Discipline(SoSDiscipline):
             series_to_add.append(forest_surface)
 
             new_chart = TwoAxesInstanciatedChart('years', LandUseV1Discipline.GLOBAL_CHARTS,
-                                                    chart_name=LandUseV1Discipline.GLOBAL_CHARTS, stacked_bar=True)
-            for serie in series_to_add:    
+                                                 chart_name=LandUseV1Discipline.GLOBAL_CHARTS, stacked_bar=True)
+            for serie in series_to_add:
                 new_chart.add_series(serie)
 
             instanciated_charts.append(new_chart)
