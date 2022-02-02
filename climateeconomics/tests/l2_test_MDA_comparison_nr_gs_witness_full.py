8d5c0a6c26574f06fa2e654dbe505dffd1510a8e
Valentin JONCQUIERES <valentin.joncquieres.external@airbus.com>
2022-02-02 14:01:40 +0100

    Rename l1 test comparison NR and GS useful only for L2 tests

--- climateeconomics/tests/l2_test_MDA_comparison_nr_gs_witness_full.py
+++ climateeconomics/tests/l2_test_MDA_comparison_nr_gs_witness_full.py
@@ -66,7 +66,7 @@ class TestComparisonNR_GS(unittest.TestCase):
         for dict_v in values_dict:
             full_values_dict.update(dict_v)
 
-        full_values_dict[f'{usecase.study_name}.sub_mda_class'] = 'MDANewtonRaphson'
+        full_values_dict[f'{usecase.study_name}.sub_mda_class'] = 'GSNewtonMDA'
         full_values_dict[f'{usecase.study_name}.linearization_mode'] = 'adjoint'
         full_values_dict[f'{usecase.study_name}.tolerance'] = 1e-7
         full_values_dict[f'{usecase.study_name}.relax_factor'] = 0.99
@@ -108,4 +108,37 @@ class TestComparisonNR_GS(unittest.TestCase):
             for value1, value2 in zip(value_nr, value_gs):
                 self.assertAlmostEqual(
                     value1, value2, msg=f'{output} values are different', delta=1.0e-4)
-        # diff.to_csv('diff_energy.csv')
+
+        ####################
+        self.name = 'Test3'
+        self.ee3 = ExecutionEngine(self.name)
+        builder3 = self.ee3.factory.get_builder_from_process(
+            repo, 'witness')
+
+        self.ee3.factory.set_builders_to_coupling_builder(builder3)
+        self.ee3.configure()
+        usecase = Study(execution_engine=self.ee3)
+        usecase.study_name = self.name
+        values_dict = usecase.setup_usecase()
+
+        full_values_dict = {}
+        for dict_v in values_dict:
+            full_values_dict.update(dict_v)
+
+        full_values_dict[f'{usecase.study_name}.sub_mda_class'] = 'GSPureNewtonMDA'
+        full_values_dict[f'{usecase.study_name}.max_mda_iter'] = 200
+        full_values_dict[f'{usecase.study_name}.linearization_mode'] = 'adjoint'
+        full_values_dict[f'{usecase.study_name}.tolerance'] = 1e-7
+        full_values_dict[f'{usecase.study_name}.relax_factor'] = 0.99
+        self.ee3.load_study_from_input_dict(full_values_dict)
+
+        # self.ee2.display_treeview_nodes()
+        self.ee3.execute()
+
+        for output in self.obj_const:
+            value_nr = self.ee.dm.get_value(
+                f'Test.{output}')
+            value_gs = self.ee3.dm.get_value(f'Test2.{output}')
+            for value1, value2 in zip(value_nr, value_gs):
+                self.assertAlmostEqual(
+                    value1, value2, msg=f'{output} values are different', delta=1.0e-4)
