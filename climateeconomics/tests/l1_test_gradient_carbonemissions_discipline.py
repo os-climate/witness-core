7815549290c75e6fe6eed1cd42319780abbf5a1e
BEKKALI Yasser <NG871F3@eu.airbus.corp>
2022-02-21 14:03:23 +0100

    update test carbon emission

--- climateeconomics/tests/l1_test_gradient_carbonemissions_discipline.py
+++ climateeconomics/tests/l1_test_gradient_carbonemissions_discipline.py
@@ -25,7 +25,7 @@ from sos_trades_core.tests.core.abstract_jacobian_unit_test import AbstractJacob
 
 
 class CarbonEmissionsJacobianDiscTest(AbstractJacobianUnittest):
-    # AbstractJacobianUnittest.DUMP_JACOBIAN = True
+    AbstractJacobianUnittest.DUMP_JACOBIAN = False
     # np.set_printoptions(threshold=np.inf)
 
     def setUp(self):
@@ -44,7 +44,9 @@ class CarbonEmissionsJacobianDiscTest(AbstractJacobianUnittest):
         self.model_name = 'carbonemission'
         ns_dict = {'ns_witness': f'{self.name}',
                    'ns_energy_mix': f'{self.name}',
-                   'ns_ref': f'{self.name}'}
+                   'ns_ref': f'{self.name}',
+                   'ns_ccs': f'{self.name}',
+                   'ns_energy': f'{self.name}'}
         self.ee.ns_manager.add_ns_def(ns_dict)
 
         mod_path = 'climateeconomics.sos_wrapping.sos_wrapping_witness.carbonemissions.carbonemissions_discipline.CarbonemissionsDiscipline'
@@ -71,10 +73,33 @@ class CarbonEmissionsJacobianDiscTest(AbstractJacobianUnittest):
         energy_supply_df_y = energy_supply_df_y.rename(
             columns={'total_CO2_emitted': 'Total CO2 emissions'})
 
+        co2_emissions_ccus_Gt = pd.DataFrame()
+        co2_emissions_ccus_Gt['years'] = energy_supply_df_y["years"]
+        co2_emissions_ccus_Gt['carbon_storage Limited by capture (Gt)'] = 0.02
+
+        CO2_emissions_by_use_sources = pd.DataFrame()
+        CO2_emissions_by_use_sources['years'] = energy_supply_df_y["years"]
+        CO2_emissions_by_use_sources['CO2 from energy mix (Gt)'] = 0.0
+        CO2_emissions_by_use_sources['carbon_capture from energy mix (Gt)'] = 0.0
+        CO2_emissions_by_use_sources['Total CO2 by use (Gt)'] = 20.0
+        CO2_emissions_by_use_sources['Total CO2 from Flue Gas (Gt)'] = 3.2
+
+        CO2_emissions_by_use_sinks = pd.DataFrame()
+        CO2_emissions_by_use_sinks['years'] = energy_supply_df_y["years"]
+        CO2_emissions_by_use_sinks['CO2 removed by energy mix (Gt)'] = 0.0
+
+        co2_emissions_needed_by_energy_mix = pd.DataFrame()
+        co2_emissions_needed_by_energy_mix['years'] = energy_supply_df_y["years"]
+        co2_emissions_needed_by_energy_mix[
+            'carbon_capture needed by energy mix (Gt)'] = 0.0
         # put manually the index
         years = np.arange(year_start, 2101)
         economics_df_y.index = years
         energy_supply_df_y.index = years
+        co2_emissions_ccus_Gt.index = years
+        CO2_emissions_by_use_sources.index = years
+        CO2_emissions_by_use_sinks.index = years
+        co2_emissions_needed_by_energy_mix.index = years
 
         CO2_emitted_forest = pd.DataFrame()
         emission_forest = np.linspace(0.01, 0.10, len(years))
@@ -84,25 +109,32 @@ class CarbonEmissionsJacobianDiscTest(AbstractJacobianUnittest):
 
         values_dict = {f'{self.name}.economics_df': economics_df_y,
                        f'{self.name}.co2_emissions_Gt': energy_supply_df_y,
-                       f'{self.name}.CO2_emitted_forest_df': CO2_emitted_forest, }
+                       f'{self.name}.CO2_emitted_forest_df': CO2_emitted_forest,
+                       f'{self.name}.co2_emissions_ccus_Gt': co2_emissions_ccus_Gt,
+                       f'{self.name}.CO2_emissions_by_use_sources': CO2_emissions_by_use_sources,
+                       f'{self.name}.CO2_emissions_by_use_sinks': CO2_emissions_by_use_sinks,
+                       f'{self.name}.co2_emissions_needed_by_energy_mix': co2_emissions_needed_by_energy_mix}
 
         self.ee.load_study_from_input_dict(values_dict)
         disc_techno = self.ee.root_process.sos_disciplines[0]
 
-        self.check_jacobian(location=dirname(__file__), filename=f'jacobian_carbon_emission_discipline.pkl', 
+        self.check_jacobian(location=dirname(__file__), filename=f'jacobian_carbon_emission_discipline.pkl',
                             discipline=disc_techno, step=1e-15, derr_approx='complex_step',
-                            inputs=[f'{self.name}.economics_df', 
-                                    f'{self.name}.co2_emissions_Gt', 
-                                    f'{self.name}.CO2_emitted_forest_df'],
-                            outputs=[f'{self.name}.CO2_emissions_df', 
-                                    f'{self.name}.CO2_objective'])
+                            inputs=[f'{self.name}.economics_df',
+                                    f'{self.name}.CO2_emissions_by_use_sources',
+                                    f'{self.name}.CO2_emitted_forest_df',
+                                    f'{self.name}.CO2_emissions_by_use_sinks', f'{self.name}.co2_emissions_needed_by_energy_mix', f'{self.name}.co2_emissions_ccus_Gt'],
+                            outputs=[f'{self.name}.CO2_emissions_df',
+                                     f'{self.name}.CO2_objective', f'{self.name}.co2_emissions_Gt'])
 
     def test_co2_objective_limit_grad(self):
 
         self.model_name = 'carbonemission'
         ns_dict = {'ns_witness': f'{self.name}',
                    'ns_energy_mix': f'{self.name}',
-                   'ns_ref': f'{self.name}'}
+                   'ns_ref': f'{self.name}',
+                   'ns_ccs': f'{self.name}',
+                   'ns_energy': f'{self.name}'}
         self.ee.ns_manager.add_ns_def(ns_dict)
 
         mod_path = 'climateeconomics.sos_wrapping.sos_wrapping_witness.carbonemissions.carbonemissions_discipline.CarbonemissionsDiscipline'
@@ -128,7 +160,25 @@ class CarbonEmissionsJacobianDiscTest(AbstractJacobianUnittest):
         energy_supply_df_y["years"] = energy_supply_df_all['years']
         energy_supply_df_y = energy_supply_df_y.rename(
             columns={'total_CO2_emitted': 'Total CO2 emissions'})
+        co2_emissions_ccus_Gt = pd.DataFrame()
+        co2_emissions_ccus_Gt['years'] = energy_supply_df_y["years"]
+        co2_emissions_ccus_Gt['carbon_storage Limited by capture (Gt)'] = 0.02
 
+        CO2_emissions_by_use_sources = pd.DataFrame()
+        CO2_emissions_by_use_sources['years'] = energy_supply_df_y["years"]
+        CO2_emissions_by_use_sources['CO2 from energy mix (Gt)'] = 0.0
+        CO2_emissions_by_use_sources['carbon_capture from energy mix (Gt)'] = 0.0
+        CO2_emissions_by_use_sources['Total CO2 by use (Gt)'] = 20.0
+        CO2_emissions_by_use_sources['Total CO2 from Flue Gas (Gt)'] = 3.2
+
+        CO2_emissions_by_use_sinks = pd.DataFrame()
+        CO2_emissions_by_use_sinks['years'] = energy_supply_df_y["years"]
+        CO2_emissions_by_use_sinks['CO2 removed by energy mix (Gt)'] = 0.0
+
+        co2_emissions_needed_by_energy_mix = pd.DataFrame()
+        co2_emissions_needed_by_energy_mix['years'] = energy_supply_df_y["years"]
+        co2_emissions_needed_by_energy_mix[
+            'carbon_capture needed by energy mix (Gt)'] = 0.0
         # put manually the index
         years = np.arange(2020, 2101)
         economics_df_y.index = years
@@ -144,15 +194,20 @@ class CarbonEmissionsJacobianDiscTest(AbstractJacobianUnittest):
 
         values_dict = {f'{self.name}.economics_df': economics_df_y,
                        f'{self.name}.co2_emissions_Gt': energy_supply_df_y,
-                       f'{self.name}.CO2_emitted_forest_df': CO2_emitted_forest, }
+                       f'{self.name}.CO2_emitted_forest_df': CO2_emitted_forest,
+                       f'{self.name}.co2_emissions_ccus_Gt': co2_emissions_ccus_Gt,
+                       f'{self.name}.CO2_emissions_by_use_sources': CO2_emissions_by_use_sources,
+                       f'{self.name}.CO2_emissions_by_use_sinks': CO2_emissions_by_use_sinks,
+                       f'{self.name}.co2_emissions_needed_by_energy_mix': co2_emissions_needed_by_energy_mix}
 
         self.ee.load_study_from_input_dict(values_dict)
         disc_techno = self.ee.root_process.sos_disciplines[0]
 
-        self.check_jacobian(location=dirname(__file__), filename=f'jacobian_co2_objective_limit.pkl', 
-                            discipline=disc_techno, step=1e-15, derr_approx='complex_step', 
-                            inputs=[f'{self.name}.economics_df', 
-                                    f'{self.name}.co2_emissions_Gt', 
-                                    f'{self.name}.CO2_emitted_forest_df'],
+        self.check_jacobian(location=dirname(__file__), filename=f'jacobian_co2_objective_limit.pkl',
+                            discipline=disc_techno, step=1e-15, derr_approx='complex_step',
+                            inputs=[f'{self.name}.economics_df',
+                                    f'{self.name}.CO2_emissions_by_use_sources',
+                                    f'{self.name}.CO2_emitted_forest_df',
+                                    f'{self.name}.CO2_emissions_by_use_sinks', f'{self.name}.co2_emissions_needed_by_energy_mix', f'{self.name}.co2_emissions_ccus_Gt'],
                             outputs=[f'{self.name}.CO2_emissions_df',
-                                    f'{self.name}.CO2_objective'])
+                                     f'{self.name}.CO2_objective', f'{self.name}.co2_emissions_Gt'])
