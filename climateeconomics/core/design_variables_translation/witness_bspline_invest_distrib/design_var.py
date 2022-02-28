08a46aa337f6fc85d1471083661adf65353b6dd5
Colin MIETKA <colin.c.mietka.external@airbus.com>
2022-02-28 08:14:25 +0100

    generic design var remove split names update energyprocessV0 accordingly rework l1 test various improvements

--- climateeconomics/core/design_variables_translation/witness_bspline_invest_distrib/design_var.py
+++ climateeconomics/core/design_variables_translation/witness_bspline_invest_distrib/design_var.py
@@ -47,13 +47,12 @@ class Design_var(object):
         years = arange(self.year_start, self.year_end + 1, self.time_step)
         list_t_years = np.linspace(0.0, 1.0, len(years))
 
-        for full_elem in list_ctrl:
-            elem = full_elem.split('.')[-1]
+        for elem in list_ctrl:
             l_activated = self.dspace.loc[self.dspace[self.VARIABLES]
                                           == elem, self.ACTIVATED_ELEM_LIST].to_list()[0]
             value_dv = self.dspace.loc[self.dspace[self.VARIABLES]
                                        == elem, self.VALUE].to_list()[0]
-            elem_val = inputs_dict[full_elem]
+            elem_val = inputs_dict[elem]
             index_false = None
             if not all(l_activated):
                 index_false = l_activated.index(False)
@@ -61,20 +60,18 @@ class Design_var(object):
                 elem_val.insert(index_false, value_dv[index_false])
                 elem_val = np.asarray(elem_val)
 
-            if len(inputs_dict[full_elem]) == len(years):
-                self.bspline_dict[full_elem] = {
-                    'bspline': None, 'eval_t': inputs_dict[full_elem], 'b_array': np.identity(len(years))}
+            if len(inputs_dict[elem]) == len(years):
+                self.bspline_dict[elem] = {
+                    'bspline': None, 'eval_t': inputs_dict[elem], 'b_array': np.identity(len(years))}
             else:
                 bspline = BSpline(n_poles=len(elem_val))
                 bspline.set_ctrl_pts(elem_val)
                 eval_t, b_array = bspline.eval_list_t(list_t_years)
                 b_array = bspline.update_b_array(b_array, index_false)
 
-                self.bspline_dict[full_elem] = {
+                self.bspline_dict[elem] = {
                     'bspline': bspline, 'eval_t': eval_t, 'b_array': b_array}
         #######
-
-        output_dataframes = {}
 
         # loop over output_descriptor to build output
         for key in self.output_descriptor.keys():
@@ -84,14 +81,11 @@ class Design_var(object):
             if out_type == 'array':
                 self.output_dict[out_name] = self.bspline_dict[key]['eval_t']
             elif out_type == 'dataframe':
-                if self.output_descriptor[key]['out_name'] not in output_dataframes.keys():
+                if self.output_descriptor[key]['out_name'] not in self.output_dict.keys():
                     # init output dataframes with 'years' index
-                    output_dataframes[out_name] = DataFrame({'years': years}, index=years)
+                    self.output_dict[out_name] = DataFrame({'years': years}, index=years)
+
                 col_name = self.output_descriptor[key]['key']
-                output_dataframes[out_name][col_name] = self.bspline_dict[key]['eval_t']
+                self.output_dict[out_name][col_name] = self.bspline_dict[key]['eval_t']
             else:
                 raise(ValueError('Output type not yet supported'))
-
-        # set output dataframes
-        for dataframe in output_dataframes.keys():
-            self.output_dict[dataframe] = output_dataframes[dataframe]
