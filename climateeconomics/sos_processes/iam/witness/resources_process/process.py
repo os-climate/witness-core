43364e421edf463837d63676c5dc53f66f6f4284
Julien Souchard <NG92D02@TOLX302085985.tls.fr.eu.airbus.corp>
2022-03-04 16:08:23 +0100

    Link resource names to process

--- climateeconomics/sos_processes/iam/witness/resources_process/process.py
+++ climateeconomics/sos_processes/iam/witness/resources_process/process.py
@@ -15,6 +15,10 @@ limitations under the License.
 '''
 
 from sos_trades_core.sos_processes.base_process_builder import BaseProcessBuilder
+from climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_coal_resource.coal_resource_model.coal_resource_disc import CoalDiscipline
+from climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_oil_resource.oil_resource_model.oil_resource_disc import OilDiscipline
+from climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_gas_resource.gas_resource_model.gas_resource_disc import GasDiscipline
+from climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_uranium_resource.uranium_resource_model.uranium_resource_disc import UraniumDiscipline
 
 
 class ProcessBuilder(BaseProcessBuilder):
@@ -27,23 +31,28 @@ class ProcessBuilder(BaseProcessBuilder):
         'version': '',
     }
 
+    COAL_NAME = CoalDiscipline.resource_name
+    OIL_NAME = OilDiscipline.resource_name
+    NATURAL_GAS_NAME = GasDiscipline.resource_name
+    URANIUM_NAME = UraniumDiscipline.resource_name
+
     def get_builders(self):
 
         ns_scatter = self.ee.study_name + '.Resources'
 
-        ns_dict = {'coal_resource': f'{ns_scatter}.coal_resource',
-                   'oil_resource': f'{ns_scatter}.oil_resource',
-                   'natural_gas_resource': f'{ns_scatter}.natural_gas_resource',
-                   'uranium_resource': f'{ns_scatter}.uranium_resource',
+        ns_dict = {'coal_resource': f'{ns_scatter}.{self.COAL_NAME}',
+                   'oil_resource': f'{ns_scatter}.{self.OIL_NAME}',
+                   'natural_gas_resource': f'{ns_scatter}.{self.NATURAL_GAS_NAME}',
+                   'uranium_resource': f'{ns_scatter}.{self.URANIUM_NAME}',
                    'ns_public': self.ee.study_name,
                    'ns_resource': ns_scatter,
                    'ns_demand': ns_scatter
                    }
         #'oil_availability_and_price': f'{ns_scatter}.oil.oil_availability_and_price',
-        mods_dict = {'Resources.coal_resource': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_coal_resource.coal_resource_model.coal_resource_disc.CoalDiscipline',
-                     'Resources.oil_resource': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_oil_resource.oil_resource_model.oil_resource_disc.OilDiscipline',
-                     'Resources.natural_gas_resource': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_gas_resource.gas_resource_model.gas_resource_disc.GasDiscipline',
-                     'Resources.uranium_resource': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_uranium_resource.uranium_resource_model.uranium_resource_disc.UraniumDiscipline',
+        mods_dict = {f'Resources.{self.COAL_NAME}': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_coal_resource.coal_resource_model.coal_resource_disc.CoalDiscipline',
+                     f'Resources.{self.OIL_NAME}': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_oil_resource.oil_resource_model.oil_resource_disc.OilDiscipline',
+                     f'Resources.{self.NATURAL_GAS_NAME}': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_gas_resource.gas_resource_model.gas_resource_disc.GasDiscipline',
+                     f'Resources.{self.URANIUM_NAME}': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_uranium_resource.uranium_resource_model.uranium_resource_disc.UraniumDiscipline',
                      'Resources': 'climateeconomics.sos_wrapping.sos_wrapping_resources.sos_wrapping_all_resource.all_resource_model.all_resource_disc.AllResourceDiscipline'
                      }
         #chain_builders_resource = self.ee.factory.get_builder_from_module()
