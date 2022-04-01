43011a2e5f6862c9b554fce11c47cd1f17c8eda5
FLORIO Thomas <NG8B00C@eu.airbus.corp>
2022-04-01 15:15:49 +0200

    add lost capital to forest + gradient + start work on lost objective disc

--- climateeconomics/tests/l0_test_lost_capital_obj.py
+++ climateeconomics/tests/l0_test_lost_capital_obj.py
@@ -35,7 +35,8 @@ class LostCapitalObjDiscTest(unittest.TestCase):
         ns_dict = {'ns_witness': f'{self.name}',
                    'ns_energy': f'{self.name}.EnergyMix',
                    'ns_ref': f'{self.name}',
-                   'ns_ccs': f'{self.name}.CCUS', }
+                   'ns_ccs': f'{self.name}.CCUS',
+                   'ns_forest': f'{self.name}.Forest', }
 
         self.ee.ns_manager.add_ns_def(ns_dict)
 
@@ -54,6 +55,7 @@ class LostCapitalObjDiscTest(unittest.TestCase):
         loss_ub = 22
         loss_rf = 16
         loss_ft = 4
+        loss_ref = 3
         lost_capital_fg = pd.DataFrame({'years': np.arange(year_start, year_end + 1),
                                         'FossilGas': loss_fg})
         lost_capital_ub = pd.DataFrame({'years': np.arange(year_start, year_end + 1),
@@ -64,15 +66,19 @@ class LostCapitalObjDiscTest(unittest.TestCase):
                                         'FischerTropsch': loss_ft})
         lost_capital_ct = pd.DataFrame({'years': np.arange(year_start, year_end + 1),
                                         'CC_tech': loss_ct})
+        lost_capital_ref = pd.DataFrame({'years': np.arange(year_start, year_end + 1),
+                                         'reforestation': loss_ref})
         lost_capital_obj_ref = 100.
         values_dict = {f'{self.name}.year_start': year_start,
                        f'{self.name}.year_end': year_end,
                        f'{self.name}.lost_capital_obj_ref': lost_capital_obj_ref,
                        f'{self.name}.energy_list': ['fuel.liquid_fuel', 'methane'],
                        f'{self.name}.ccs_list': ['carbon_capture'],
+                       f'{self.name}.biomass_list': ['biomass_dry'],
                        f'{self.name}.EnergyMix.methane.technologies_list': ['FossilGas', 'UpgradingBiogas'],
                        f'{self.name}.EnergyMix.fuel.liquid_fuel.technologies_list': ['Refinery', 'FischerTropsch'],
                        f'{self.name}.CCUS.carbon_capture.technologies_list': ['CC_tech'],
+                       f'{self.name}.Forest.biomass_dry.technologies_list': ['reforestation'],
                        f'{self.name}.CCUS.carbon_capture.CC_tech.lost_capital': lost_capital_ct,
                        f'{self.name}.EnergyMix.methane.FossilGas.lost_capital': lost_capital_fg,
                        f'{self.name}.EnergyMix.methane.UpgradingBiogas.lost_capital': lost_capital_ub,
@@ -82,7 +88,9 @@ class LostCapitalObjDiscTest(unittest.TestCase):
                        f'{self.name}.EnergyMix.methane.FossilGas.techno_capital': lost_capital_fg,
                        f'{self.name}.EnergyMix.methane.UpgradingBiogas.techno_capital': lost_capital_ub,
                        f'{self.name}.EnergyMix.fuel.liquid_fuel.Refinery.techno_capital': lost_capital_rf,
-                       f'{self.name}.EnergyMix.fuel.liquid_fuel.FischerTropsch.techno_capital': lost_capital_ft, }
+                       f'{self.name}.EnergyMix.fuel.liquid_fuel.FischerTropsch.techno_capital': lost_capital_ft,
+                       f'{self.name}.Forest.biomass_dry.reforestation.lost_capital': lost_capital_ref,
+                       f'{self.name}.Forest.biomass_dry.reforestation.techno_capital': lost_capital_ref, }
 
         self.ee.load_study_from_input_dict(values_dict)
 
@@ -92,7 +100,8 @@ class LostCapitalObjDiscTest(unittest.TestCase):
             f'{self.name}.{self.model_name}.lost_capital_df')
         lost_capital_objective = self.ee.dm.get_value(
             f'{self.name}.lost_capital_objective')
-        sum_lost_capital_th = loss_fg + loss_ub + loss_rf + loss_ft + loss_ct
+        sum_lost_capital_th = loss_fg + loss_ub + \
+            loss_rf + loss_ft + loss_ct + loss_ref
         self.assertListEqual([sum_lost_capital_th] * (year_end - year_start + 1),
                              lost_capital_df['Sum of lost capital'].values.tolist())
 
@@ -103,5 +112,5 @@ class LostCapitalObjDiscTest(unittest.TestCase):
             f'{self.name}.{self.model_name}')[0]
         filter = disc.get_chart_filter_list()
         graph_list = disc.get_post_processing_list(filter)
-#         for graph in graph_list:
-#             graph.to_plotly().show()
+        for graph in graph_list:
+            graph.to_plotly().show()
