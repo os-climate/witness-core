8f7b0d322fe8b99d0a7b34ec1342cc817b4ea8e9
Valentin JONCQUIERES <valentin.joncquieres.external@airbus.com>
2022-04-15 16:00:25 +0200

    Add forcing_df for post-procs + post procs

--- climateeconomics/sos_processes/iam/witness/climate_process_ms/process.py
+++ climateeconomics/sos_processes/iam/witness/climate_process_ms/process.py
@@ -59,4 +59,7 @@ class ProcessBuilder(BaseProcessBuilder):
         multi_scenario = self.ee.factory.create_very_simple_multi_scenario_builder(
             scatter_scenario_name, 'scenario_list', builder_cdf_list, autogather=True, gather_node='Post-processing')
 
+        self.ee.post_processing_manager.add_post_processing_module_to_namespace('ns_post_processing',
+                                                                                'climateeconomics.sos_wrapping.sos_wrapping_witness.post_proc_climate_ms.post_processing_climate')
+
         return multi_scenario
