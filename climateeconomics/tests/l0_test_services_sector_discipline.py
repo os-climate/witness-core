35a79b9c27379dbc388a5781a381402eb1620780
Marie MORERE <marie.morere@airbus.com>
2022-05-11 14:52:16 +0200

    Update units

--- climateeconomics/tests/l0_test_services_sector_discipline.py
+++ climateeconomics/tests/l0_test_services_sector_discipline.py
@@ -120,5 +120,5 @@ class ServicesDiscTest(unittest.TestCase):
             f'{self.name}.{self.model_name}')[0]
         filterr = disc.get_chart_filter_list()
         graph_list = disc.get_post_processing_list(filterr)
-        for graph in graph_list:
-            graph.to_plotly().show()
+#         for graph in graph_list:
+#             graph.to_plotly().show()
