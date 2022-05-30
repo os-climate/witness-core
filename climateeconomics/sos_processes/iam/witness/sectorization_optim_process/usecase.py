22c80d19e7a96694c3cb71ef572ae42c5a762674
Marie MORERE <marie.morere@airbus.com>
2022-05-30 15:52:14 +0200

    Sectorizaiton optim process

--- climateeconomics/sos_processes/iam/witness/sectorization_optim_process/usecase.py
+++ climateeconomics/sos_processes/iam/witness/sectorization_optim_process/usecase.py
@@ -100,53 +100,53 @@ class Study(StudyManager):
  
         dspace = pd.DataFrame(dspace_dict)
 
-        design_var_descriptor = {'output_alpha_services_in': {'out_name': 'output_alpha','out_type': 'float', 'index': arange(0), 
+        design_var_descriptor = {'output_alpha_services_in': {'out_name': 'output_alpha','out_type': 'float', 'index': arange(1), 
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_services'},
-                                'prod_gr_start_services_in': {'out_name': 'productivity_gr_start','out_type': 'float', 'index': arange(0),
+                                'prod_gr_start_services_in': {'out_name': 'productivity_gr_start','out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_services'},
-                                'decl_rate_tfp_services_in': {'out_name': 'decline_rate_tfp','out_type': 'float', 'index': arange(0),
+                                'decl_rate_tfp_services_in': {'out_name': 'decline_rate_tfp','out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_services'},
-                                'prod_start_services_in': {'out_name': 'productivity_start', 'out_type': 'float','index': arange(0),
+                                'prod_start_services_in': {'out_name': 'productivity_start', 'out_type': 'float','index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_services'},
-                                'energy_eff_k_services_in': {'out_name': 'energy_eff_k', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_k_services_in': {'out_name': 'energy_eff_k', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_services'},
-                                'energy_eff_cst_services_in': {'out_name': 'energy_eff_cst', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_cst_services_in': {'out_name': 'energy_eff_cst', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_services'},
-                                'energy_eff_xzero_services_in': {'out_name': 'energy_eff_xzero', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_xzero_services_in': {'out_name': 'energy_eff_xzero', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_services'},
-                                'energy_eff_max_services_in': {'out_name': 'energy_eff_max', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_max_services_in': {'out_name': 'energy_eff_max', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_services'},
-                                'output_alpha_agri_in': {'out_name': 'output_alpha','out_type': 'float', 'index': arange(0),
+                                'output_alpha_agri_in': {'out_name': 'output_alpha','out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_agri'},
-                                'prod_gr_start_agri_in': {'out_name': 'productivity_gr_start','out_type': 'float', 'index': arange(0),
+                                'prod_gr_start_agri_in': {'out_name': 'productivity_gr_start','out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_agri'},
-                                'decl_rate_tfp_agri_in': {'out_name': 'decline_rate_tfp','out_type': 'float', 'index': arange(0),
+                                'decl_rate_tfp_agri_in': {'out_name': 'decline_rate_tfp','out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_agri'},
-                                'prod_start_agri_in': {'out_name': 'productivity_start', 'out_type': 'float', 'index': arange(0),
+                                'prod_start_agri_in': {'out_name': 'productivity_start', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_agri'},
-                                'energy_eff_k_agri_in': {'out_name': 'energy_eff_k', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_k_agri_in': {'out_name': 'energy_eff_k', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_agri'},
-                                'energy_eff_cst_agri_in': {'out_name': 'energy_eff_cst', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_cst_agri_in': {'out_name': 'energy_eff_cst', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_agri'},
-                                'energy_eff_xzero_agri_in': {'out_name': 'energy_eff_xzero', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_xzero_agri_in': {'out_name': 'energy_eff_xzero', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_agri'},
-                                'energy_eff_max_agri_in': {'out_name': 'energy_eff_max', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_max_agri_in': {'out_name': 'energy_eff_max', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_agri'},
-                                'output_alpha_indus_in': {'out_name': 'output_alpha','out_type': 'float', 'index': arange(0),
+                                'output_alpha_indus_in': {'out_name': 'output_alpha','out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_indus'},
-                                'prod_gr_start_indus_in': {'out_name': 'productivity_gr_start','out_type': 'float', 
+                                'prod_gr_start_indus_in': {'out_name': 'productivity_gr_start','out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_indus'},
-                                'decl_rate_tfp_indus_in': {'out_name': 'decline_rate_tfp','out_type': 'float', 'index': arange(0),
+                                'decl_rate_tfp_indus_in': {'out_name': 'decline_rate_tfp','out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_indus'},
-                                'prod_start_indus_in': {'out_name': 'productivity_start', 'out_type': 'float', 'index': arange(0),
+                                'prod_start_indus_in': {'out_name': 'productivity_start', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_indus'},
-                                'energy_eff_k_indus_in': {'out_name': 'energy_eff_k', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_k_indus_in': {'out_name': 'energy_eff_k', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_indus'},
-                                'energy_eff_cst_indus_in': {'out_name': 'energy_eff_cst', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_cst_indus_in': {'out_name': 'energy_eff_cst', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_indus'},
-                                'energy_eff_xzero_indus_in': {'out_name': 'energy_eff_xzero', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_xzero_indus_in': {'out_name': 'energy_eff_xzero', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_indus'},
-                                'energy_eff_max_indus_in': {'out_name': 'energy_eff_max', 'out_type': 'float', 'index': arange(0),
+                                'energy_eff_max_indus_in': {'out_name': 'energy_eff_max', 'out_type': 'float', 'index': arange(1),
                                                               'namespace_in': 'ns_optim','namespace_out': 'ns_indus'},         
                              }
 
@@ -200,12 +200,16 @@ class Study(StudyManager):
             columns=['variable', 'ftype', 'weight', AGGR_TYPE, 'namespace'])
         func_df['variable'] = [ 'error_pib_total', 'error_cap_total', 
                                'Industry.cap_error', 'Agriculture.cap_error', 'Services.cap_error',
-                               'Industry.gdp_error', 'Agriculture.gdp_error', 'Services.gdp_error']
-        func_df['ftype'] =  [OBJECTIVE,OBJECTIVE, OBJECTIVE,OBJECTIVE, OBJECTIVE,OBJECTIVE, OBJECTIVE,OBJECTIVE]
-        func_df['weight'] = [1,1,1,1,1,1,1,1]
+                               'Industry.gdp_error', 'Agriculture.gdp_error', 'Services.gdp_error',
+                               'Industry.energy_eff_error', 'Agriculture.energy_eff_error', 'Services.energy_eff_error']
+        func_df['ftype'] =  [OBJECTIVE,OBJECTIVE, OBJECTIVE,OBJECTIVE, OBJECTIVE,OBJECTIVE, OBJECTIVE,OBJECTIVE,
+                             OBJECTIVE, OBJECTIVE, OBJECTIVE]
+        func_df['weight'] = [1,1,1,1,1,1,1,1,1,1,1]
         func_df[AGGR_TYPE] = [AGGR_TYPE_SUM, AGGR_TYPE_SUM, AGGR_TYPE_SUM, AGGR_TYPE_SUM, 
-                              AGGR_TYPE_SUM, AGGR_TYPE_SUM,AGGR_TYPE_SUM, AGGR_TYPE_SUM]
-        func_df['namespace'] = ['ns_obj', 'ns_obj', 'ns_obj', 'ns_obj', 'ns_obj', 'ns_obj', 'ns_obj', 'ns_obj']
+                              AGGR_TYPE_SUM, AGGR_TYPE_SUM,AGGR_TYPE_SUM, AGGR_TYPE_SUM,
+                              AGGR_TYPE_SUM,AGGR_TYPE_SUM, AGGR_TYPE_SUM]
+        func_df['namespace'] = ['ns_obj', 'ns_obj', 'ns_obj', 'ns_obj', 'ns_obj', 'ns_obj', 'ns_obj', 'ns_obj',
+                                'ns_obj', 'ns_obj', 'ns_obj']
         func_mng_name = 'FunctionsManager'
 
         prefix = f'{ns_coupling}.{func_mng_name}.'
@@ -219,9 +223,11 @@ class Study(StudyManager):
             dirname(dirname(dirname(dirname(dirname(__file__))))), 'tests', 'data/sectorization_fitting')
         hist_gdp = pd.read_csv(join(data_dir, 'hist_gdp_sect.csv'))
         hist_capital = pd.read_csv(join(data_dir, 'hist_capital_sect.csv'))
+        hist_energy = pd.read_csv(join(data_dir, 'hist_energy_sect.csv'))
         sect_input = {}
         sect_input[ns_coupling + self.obj_name +  '.historical_gdp'] = hist_gdp
         sect_input[ns_coupling + self.obj_name +  '.historical_capital'] = hist_capital
+        sect_input[ns_coupling + self.obj_name +  '.historical_energy'] = hist_energy
         disc_dict.update(sect_input)
        
     #TO BE UPDATED WITH RIGHT DATA 
