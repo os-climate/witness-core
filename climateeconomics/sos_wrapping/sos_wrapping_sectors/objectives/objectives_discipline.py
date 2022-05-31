adc39292ab9ca45eaa5f646389633a8a7e71c9a6
Marie MORERE <marie.morere@airbus.com>
2022-05-31 15:25:33 +0200

    and fix chart

--- climateeconomics/sos_wrapping/sos_wrapping_sectors/objectives/objectives_discipline.py
+++ climateeconomics/sos_wrapping/sos_wrapping_sectors/objectives/objectives_discipline.py
@@ -232,7 +232,7 @@ class ObjectivesDiscipline(ClimateEcoDiscipline):
                 hist_gdp_sector = historical_gdp[sector]
                 simu_capital_sector_df = self.get_sosdisc_inputs(f'{sector}.detailed_capital_df')
                 simu_capital_sector = simu_capital_sector_df['capital']
-                hist_capital_sector = historical_gdp[sector]
+                hist_capital_sector = historical_capital[sector]
                 hist_energy_eff_sector = hist_energy_efficiency[sector].values
                 simu_energy_eff_sector = simu_capital_sector_df['energy_efficiency']
                 
