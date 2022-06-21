29ea34ac8a27abf606f965b56a6c284359844ea9
Colin MIETKA <colin.c.mietka.external@airbus.com>
2022-06-21 10:27:11 +0200

    remove plotly show in l0 test

--- climateeconomics/tests/l0_test_temperature_v2_discipline.py
+++ climateeconomics/tests/l0_test_temperature_v2_discipline.py
@@ -79,8 +79,8 @@ class TemperatureDiscTest(unittest.TestCase):
             f'{self.name}.{self.model_name}')[0]
         filter = disc.get_chart_filter_list()
         graph_list = disc.get_post_processing_list(filter)
-        for graph in graph_list:
-            graph.to_plotly().show()
+        # for graph in graph_list:
+        #     graph.to_plotly().show()
 
     def test_execute_FUND(self):
 
@@ -133,5 +133,5 @@ class TemperatureDiscTest(unittest.TestCase):
             f'{self.name}.{self.model_name}')[0]
         filter = disc.get_chart_filter_list()
         graph_list = disc.get_post_processing_list(filter)
-        for graph in graph_list:
-            graph.to_plotly().show()
+        # for graph in graph_list:
+        #     graph.to_plotly().show()
