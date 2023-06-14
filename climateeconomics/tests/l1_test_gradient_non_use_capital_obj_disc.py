5bcd987398bc740a60035c553924983a4130e033
Valentin Joncquieres <valentin.joncquieres@capgemini.com>
2023-06-14 12:47:10 +0200

    Comment some dump jacobian

--- climateeconomics/tests/l1_test_gradient_non_use_capital_obj_disc.py
+++ climateeconomics/tests/l1_test_gradient_non_use_capital_obj_disc.py
@@ -23,7 +23,7 @@ from sostrades_core.tests.core.abstract_jacobian_unit_test import AbstractJacobi
 
 
 class NonUseCapitalObjJacobianDiscTest(AbstractJacobianUnittest):
-    AbstractJacobianUnittest.DUMP_JACOBIAN = True
+    # AbstractJacobianUnittest.DUMP_JACOBIAN = True
 
     def analytic_grad_entry(self):
         return [
