22dad2401242b870e604014f68529a16bc0c8c88
Yasser BEKKALI <yasser.bekkali@capgemini.com>
2023-07-13 17:36:47 +0200

    use clea namespaces argument to update namespace list with extra ns when needed

--- climateeconomics/sos_processes/iam/witness/witness_optim_process/process.py
+++ climateeconomics/sos_processes/iam/witness/witness_optim_process/process.py
@@ -45,7 +45,7 @@ class ProcessBuilder(WITNESSSubProcessBuilder):
 
         # modify namespaces defined in the child process
         self.ee.ns_manager.update_namespace_list_with_extra_ns(
-            optim_name, after_name=self.ee.study_name)  # optim_name
+            optim_name, after_name=self.ee.study_name, clean_namespaces = True)  # optim_name
 
         #-- set optim builder
         opt_builder = self.ee.factory.create_optim_builder(
