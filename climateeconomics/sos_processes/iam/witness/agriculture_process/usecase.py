2ab771f95ef811ec7253d78ae45533f8c62d153f
perrotcap <antoine.perrot@capgemini.com>
2023-08-31 14:46:30 +0200

    fix usecase

--- climateeconomics/sos_processes/iam/witness/agriculture_process/usecase.py
+++ climateeconomics/sos_processes/iam/witness/agriculture_process/usecase.py
@@ -72,22 +72,7 @@ class Study(StudyManager):
         years = np.arange(self.year_start, self.year_end + 1, 1)
         year_range = self.year_end - self.year_start + 1
 
-        # population = np.array(np.linspace(7900, 8500, year_range))
-
-        population = np.array(
-            [7886.69358, 7966.665211, 8045.375451, 8122.797867, 8198.756532, 8273.083818, 8345.689982, 8416.57613,
-             8485.795919, 8553.312856, 8619.058953, 8683.042395, 8745.257656,
-             8805.680119, 8864.337481, 8921.246891, 8976.395584, 9029.771873, 9081.354412, 9131.121464, 9179.083525,
-             9225.208209, 9269.477788, 9311.832053, 9352.201944, 9390.558602,
-             9426.867911, 9461.124288, 9493.330078, 9523.465887, 9551.506077, 9577.443894, 9601.291404, 9623.075287,
-             9642.80847, 9660.498856, 9676.158366, 9689.826469, 9701.54988,
-             9711.366041, 9719.318272, 9725.419042, 9729.702777, 9732.206794, 9732.922689, 9731.871402, 9729.064465,
-             9724.513081, 9718.249401, 9710.282554, 9700.610324, 9689.251038,
-             9676.243561, 9661.590658, 9645.329918, 9627.498797, 9608.104964, 9587.197508, 9564.828118, 9541.038722,
-             9515.888869, 9489.415825, 9461.693469, 9432.803085, 9402.775341,
-             9371.660258, 9339.478398, 9306.261187, 9272.043294, 9236.831993, 9200.632391, 9163.429244, 9125.227987,
-             9086.036337, 9045.87235, 9004.753844, 8962.700979, 8919.730768,
-             8875.855926, 8831.098444, 8785.553666])
+        population = np.array(np.linspace(7900, 8500, year_range))
 
         temperature = np.array(np.linspace(1.05, 5, year_range))
         #         temperature = np.array(np.linspace(1.05, 1.05, year_range))
@@ -99,7 +84,6 @@ class Study(StudyManager):
         population_df = pd.DataFrame(
             {"years": years, "population": population})
         population_df.index = years
-        """
         red_meat_percentage = np.linspace(6.82, 1, year_range)
         white_meat_percentage = np.linspace(13.95, 5, year_range)
         self.red_meat_percentage = pd.DataFrame({
@@ -108,7 +92,6 @@ class Study(StudyManager):
         self.white_meat_percentage = pd.DataFrame({
             'years': years,
             'white_meat_percentage': white_meat_percentage})
-        """
         diet_df = pd.DataFrame({'red meat': [11.02],
                                 'white meat': [31.11],
                                 'milk': [79.27],
@@ -127,10 +110,10 @@ class Study(StudyManager):
         agriculture_input[self.study_name + self.agriculture_name +
                           '.diet_df'] = diet_df
 
-        # agriculture_input[self.study_name +
-        #                   '.red_meat_percentage'] = self.red_meat_percentage
-        # agriculture_input[self.study_name +
-        #                   '.white_meat_percentage'] = self.white_meat_percentage
+        agriculture_input[self.study_name +
+                          '.red_meat_percentage'] = self.red_meat_percentage
+        agriculture_input[self.study_name +
+                           '.white_meat_percentage'] = self.white_meat_percentage
         agriculture_input[self.study_name + self.agriculture_name +
                           '.other_use_agriculture'] = other
 
@@ -176,14 +159,5 @@ if '__main__' == __name__:
     uc_cls.load_data()
     # uc_cls.execution_engine.display_treeview_nodes(display_variables=True)
     # uc_cls.execution_engine.set_debug_mode()
-    uc_cls.run()
+    uc_cls.test()
 
-    ppf = PostProcessingFactory()
-    for disc in uc_cls.execution_engine.root_process.proxy_disciplines:
-        filters = ppf.get_post_processing_filters_by_discipline(
-            disc)
-        graph_list = ppf.get_post_processing_by_discipline(
-            disc, filters, as_json=False)
-
-        # for graph in graph_list:
-        #     graph.to_plotly().show()
