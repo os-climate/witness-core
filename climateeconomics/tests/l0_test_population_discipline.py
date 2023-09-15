333250c3607508ac5d4ca4aae85abde90a5299d9
perrotcap <antoine.perrot@capgemini.com>
2023-09-15 12:23:13 +0200

    remove show graphs

--- climateeconomics/tests/l0_test_population_discipline.py
+++ climateeconomics/tests/l0_test_population_discipline.py
@@ -233,7 +233,8 @@ class PopDiscTest(unittest.TestCase):
         filter = disc.get_chart_filter_list()
         graph_list = disc.get_post_processing_list(filter)
         for graph in graph_list:
-           graph.to_plotly().show()
+           #graph.to_plotly().show()
+           pass
 
 
 if '__main__' == __name__:
