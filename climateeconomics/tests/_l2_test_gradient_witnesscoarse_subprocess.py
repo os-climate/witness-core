4cc85c18228fa9bbc38268aa1383b7df19589ac1
perrotcap <antoine.perrot@capgemini.com>
2023-09-25 17:08:21 +0200

    focus test gradient witness coarse wrt carbon capture

--- climateeconomics/tests/_l2_test_gradient_witnesscoarse_subprocess.py
+++ climateeconomics/tests/_l2_test_gradient_witnesscoarse_subprocess.py
@@ -70,8 +70,9 @@ class OptimSubprocessJacobianDiscTest(AbstractJacobianUnittest):
 
         coupling_disc = self.ee.root_process.proxy_disciplines[0]
 
-        outputs = self.ee.dm.get_all_namespaces_from_var_name(
-            'objective_lagrangian')
+        outputs = [self.ee.dm.get_all_namespaces_from_var_name(
+            'objective_lagrangian')[0],
+                   self.ee.dm.get_all_namespaces_from_var_name('negative_welfare_objective')[0]]
         inputs_name = [f'{energy}_{techno}_array_mix' for energy, techno_dict in DEFAULT_COARSE_TECHNO_DICT.items() for
                        techno in techno_dict['value']]
         inputs_name = [name.replace('.', '_') for name in inputs_name]
