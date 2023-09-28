d3066579d2a8edd27c672b486188e64dd750f33c
perrotcap <antoine.perrot@capgemini.com>
2023-09-28 17:23:11 +0200

    lower bound constraint on usable capital is active only when GDP fix

--- climateeconomics/tests/l1_test_gradient_macroeconomics_discipline.py
+++ climateeconomics/tests/l1_test_gradient_macroeconomics_discipline.py
@@ -25,7 +25,7 @@ from sostrades_core.tests.core.abstract_jacobian_unit_test import AbstractJacobi
 
 
 class MacroEconomicsJacobianDiscTest(AbstractJacobianUnittest):
-    #AbstractJacobianUnittest.DUMP_JACOBIAN = True
+    AbstractJacobianUnittest.DUMP_JACOBIAN = True
 
     def setUp(self):
 
