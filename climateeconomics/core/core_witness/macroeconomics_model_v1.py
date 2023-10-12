[fix] US0154-151: corrected gradients for Ewasted

climateeconomics/core/core_witness/macroeconomics_model_v1.py
@@ -296,7 +296,7 @@ class MacroEconomics:
         self.economics_df[GlossaryCore.UnusedEnergy] = np.maximum(net_energy_production - optimal_energy_production, 0.) * 1e3
         # Energy_wasted = max((Enet - Eoptimal),0.)
         self.economics_df[GlossaryCore.EnergyWasted] = (net_energy_production - optimal_energy_production) * 1e3 #TWh
-        self.economics_df.loc[self.economics_df[GlossaryCore.EnergyWasted] < 0.] = 0.
+        self.economics_df.loc[self.economics_df[GlossaryCore.EnergyWasted] < 0., GlossaryCore.EnergyWasted] = 0.
 
 
     def compute_energy_efficiency(self):
@@ -678,8 +678,9 @@ class MacroEconomics:
         k = np.diag(self.max_capital_utilisation_ratio / self.capital_utilisation_ratio / energy_efficiency * 1.e3)
         d_Ew_dE = self._identity_derivative() * 1.e3 - np.matmul(k, d_Kne_dE) # Enet converted from PWh to TWh
         # Since Ewasted = max(Enet - Eoptimal, 0.), gradient should be 0 when Enet - Eoptimal <=0, ie when Ewasted =0
+        # => put to 0 the lines of the gradient matrix corresponding to the years where Ewasted=0
         matrix_of_years_E_is_wasted = (self.economics_df[GlossaryCore.EnergyWasted].values > 0.).astype(int)
-        d_Ew_dE = np.multiply(matrix_of_years_E_is_wasted, d_Ew_dE)
+        d_Ew_dE = np.transpose(np.multiply(matrix_of_years_E_is_wasted, np.transpose(d_Ew_dE)))
 
         return dY_dE, d_Ku_d_E, d_lower_bound_constraint_dE, d_Ew_dE
 
@@ -741,8 +742,9 @@ class MacroEconomics:
         k = np.diag(self.max_capital_utilisation_ratio / self.capital_utilisation_ratio / energy_efficiency * 1.e3)
         d_Ew_d_wap = - np.matmul(k, d_Kne_d_wap)
         # Since Ewasted = max(Enet - Eoptimal, 0.), gradient should be 0 when Enet - Eoptimal <=0, ie when Ewasted =0
+        # => put to 0 the lines of the gradient matrix corresponding to the years where Ewasted=0
         matrix_of_years_E_is_wasted = (self.economics_df[GlossaryCore.EnergyWasted].values > 0.).astype(int)
-        d_Ew_d_wap = np.multiply(matrix_of_years_E_is_wasted, d_Ew_d_wap)
+        d_Ew_d_wap = np.transpose(np.multiply(matrix_of_years_E_is_wasted, np.transpose(d_Ew_d_wap)))
 
         return d_Ku_d_wap, d_Ew_d_wap, d_Y_d_wap, d_lower_bound_constraint_d_wap
 
@@ -971,8 +973,9 @@ class MacroEconomics:
         k = np.diag(self.max_capital_utilisation_ratio / self.capital_utilisation_ratio / energy_efficiency * 1.e3)
         d_Ew_d_dfo = - np.matmul(k, d_Kne_d_dfo)
         # Since Ewasted = max(Enet - Eoptimal, 0.), gradient should be 0 when Enet - Eoptimal <=0, ie when Ewasted =0
+        # => put to 0 the lines of the gradient matrix corresponding to the years where Ewasted=0
         matrix_of_years_E_is_wasted = (self.economics_df[GlossaryCore.EnergyWasted].values > 0.).astype(int)
-        d_Ew_d_dfo = np.multiply(matrix_of_years_E_is_wasted, d_Ew_d_dfo)
+        d_Ew_d_dfo = np.transpose(np.multiply(matrix_of_years_E_is_wasted, np.transpose(d_Ew_d_dfo)))
 
 
         return d_Y_d_dfo, d_Ku_d_dfo, d_Ew_d_dfo, d_lower_bound_constraint_d_dfo