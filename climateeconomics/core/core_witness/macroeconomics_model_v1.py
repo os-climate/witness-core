[fix] for US154-131: corrected and recomputed gradients of Ewasted_objective. l0 & l1 test pass

climateeconomics/core/core_witness/macroeconomics_model_v1.py
@@ -306,7 +306,7 @@ class MacroEconomics:
         """
         # total energy is supposed to be > 0.
         energy_wasted_objective = self.economics_df[GlossaryCore.EnergyWasted].values.sum() / \
-                                  max(1.e-10, abs(self.energy_production[GlossaryCore.TotalProductionValue].values.sum()))
+                                  self.energy_production[GlossaryCore.TotalProductionValue].values.sum()
 
         self.energy_wasted_objective = np.array([energy_wasted_objective])
 
@@ -1061,28 +1061,25 @@ class MacroEconomics:
         """derivative of net output wrt share energy_invest"""
         return self._null_derivative()
 
-    def d_partial_energy_wasted_objective_d_partial_energy_wasted(self):
-        """derivative of energy wasted objective wrt energy wasted
-        Eobj = sum_years(Ewasted)/sum_years(Etotal)
-        dEobj/dEwasted = d(sum_years(Ewasted))/dEwasted/sum_years(Etotal)
-        Eobj is a scalar
-        derivatives above are partial derivatives
+    def grad_energy_wasted_objective(self, grad_sum_energy_wasted, grad_sum_energy_total):
         """
-        d_partial_ew_obj_d_partial_ew = 1. / max(1.e-10, abs(self.energy_production[GlossaryCore.TotalProductionValue].values.sum())) \
-                        * np.ones(len(self.years_range))
-        return d_partial_ew_obj_d_partial_ew
+        gradient of the Energy_wasted objective as function of the gradients of:
+        - the sum of the energy_wasted
+        - the sum of the total energy production
+        grad(Ew_obj) = grad(sum_year(Ew)/sum_year(Etotal)) =
+        ((sum_year(Etotal) * grad(sum_year(Ewasted) - (sum_year(Ewasted) * grad(sum_year(Etotal)))/
+        (sum_year(Etotal))^2
+        where grad(sum_year(E)) = sum_year(grad(E))
+        grad(sum_year(Ewasted)) = sum_year(grad(Ewasted)) = np.ones(self.years_range) @ grad(Ewasted)
+        grad(sum_year(Etotal)) = sum_year(grad(Etotal)) = np.ones(self.years_range) @ grad(Etotal)
+        """
+        sum_ewasted = self.economics_df[GlossaryCore.EnergyWasted].values.sum()
+        sum_etotal = self.energy_production[GlossaryCore.TotalProductionValue].values.sum()
+        # sumetotal is supposed > 0 otherwise no energy in the system => cannot work
+        grad_energy_wasted_obj = (sum_etotal * grad_sum_energy_wasted - sum_ewasted * grad_sum_energy_total) / \
+                                 sum_etotal ** 2
 
-    def d_partial_energy_wasted_objective_d_parital_total_energy_prod(self):
-        """ derivative of energy wasted objective wrt total energy production
-                Eobj = sum_years(Ewasted)/sum_years(Etotal)
-        dEobj/dEtotal = sum_years(Ewasted))* -d(sum_years(Etotal))dEtotal/sum_years(Etotal)^2
-        Eobj is a scalar
-        Derivatives above are partial derivatives
-        """
-        d_partial_ew_obj_d_partial_etot = - self.economics_df[GlossaryCore.EnergyWasted].values.sum() / \
-                          (max(1.e-10, abs(self.energy_production[GlossaryCore.TotalProductionValue].values.sum()))) ** 2 \
-                          * np.ones(len(self.years_range))
-        return d_partial_ew_obj_d_partial_etot
+        return grad_energy_wasted_obj
 
     """-------------------END of Gradient functions-------------------"""
 

climateeconomics/sos_wrapping/sos_wrapping_witness/macroeconomics/macroeconomics_discipline.py
@@ -263,7 +263,7 @@ class MacroeconomicsDiscipline(ClimateEcoDiscipline):
 
         # Model execution
         economics_detail_df, economics_df, energy_investment, energy_investment_wo_renewable, \
-            workforce_df, capital_df, sector_gdp_df = \
+            workforce_df, capital_df, sector_gdp_df, energy_wasted_objective = \
             self.macro_model.compute(macro_inputs)
 
         # Store output data
@@ -276,7 +276,7 @@ class MacroeconomicsDiscipline(ClimateEcoDiscipline):
                        GlossaryCore.DetailedCapitalDfValue: capital_df,
                        GlossaryCore.CapitalDfValue: capital_df[GlossaryCore.CapitalDf['dataframe_descriptor'].keys()],
                        GlossaryCore.ConstraintLowerBoundUsableCapital: self.macro_model.delta_capital_cons,
-                       GlossaryCore.EnergyWastedObjective: self.macro_model.energy_wasted_objective,
+                       GlossaryCore.EnergyWastedObjective: energy_wasted_objective,
                        }
 
         self.store_sos_outputs_values(dict_values)
@@ -318,15 +318,10 @@ class MacroeconomicsDiscipline(ClimateEcoDiscipline):
 
         # Compute gradient for coupling variable Total production
         d_gross_output_d_energy, d_usable_capital_d_energy, d_lower_bound_constraint_dE, d_energy_wasted_d_energy = self.macro_model.d_Y_Ku_Ew_Constraint_d_energy()
-        """
-        For the objective on the Energy wasted:
-        dEobj = d_partial_Eobj/d_partial_Ew * dEw + d_partial_Eobj/d_partial_Etotal * dEtotal
-        dEobj/dEtot = d_partial_Eobj/d_partialEw * dEw/dEtotal + d_partial_Eobj/d_partial_Etotal
-        compute it here in order not to compute twice d_Y, dKu, dE_constraint/d_energy if it was done in macroeconomics_model
-        """
-        d_partial_ew_obj_d_partial_etot = self.macro_model.d_partial_energy_wasted_objective_d_parital_total_energy_prod()
-        d_partial_ew_obj_d_partial_ew = self.macro_model.d_partial_energy_wasted_objective_d_partial_energy_wasted()
-        d_ew_obj_d_etot = d_partial_ew_obj_d_partial_ew @ d_energy_wasted_d_energy + d_partial_ew_obj_d_partial_etot
+        # gradient of the energy_wasted_objective
+        d_sum_energy_wasted_d_energy_total = np.ones(nb_years) @ d_energy_wasted_d_energy
+        d_sum_energy_total_d_energy_total = np.ones(nb_years) @ np.identity(nb_years)
+        d_energy_wasted_objective_d_energy = self.macro_model.grad_energy_wasted_objective(d_sum_energy_wasted_d_energy_total, d_sum_energy_total_d_energy_total)
 
         d_net_output_d_energy = self.macro_model.d_net_output_d_user_input(d_gross_output_d_energy)
         d_energy_investment_d_energy, d_investment_d_energy, d_non_energy_investment_d_energy = self.macro_model.d_investment_d_user_input(d_net_output_d_energy)
@@ -370,7 +365,7 @@ class MacroeconomicsDiscipline(ClimateEcoDiscipline):
         self.set_partial_derivative_for_other_types(
             (GlossaryCore.EnergyWastedObjective,),
             (GlossaryCore.EnergyProductionValue, GlossaryCore.TotalProductionValue),
-            d_ew_obj_d_etot)
+            d_energy_wasted_objective_d_energy)
 
         # Compute gradient for coupling variable damage (column damage frac output)
         d_gross_output_d_damage_frac_output, d_Ku_d_dfo, d_Ew_d_dfo, d_lower_bound_constraint_d_dfo = \
@@ -385,6 +380,10 @@ class MacroeconomicsDiscipline(ClimateEcoDiscipline):
             d_net_output_d_damage_frac_output, d_investment_d_damage_frac_output)
         d_consumption_pc_d_damage_frac_output = self.macro_model.d_consumption_per_capita_d_user_input(
             d_consumption_d_damage_frac_output)
+        # gradient of the energy_wasted_objective
+        d_sum_energy_wasted_d_damage_frac_output = np.ones(nb_years) @ d_Ew_d_dfo
+        d_sum_energy_total_d_damage_frac_output = np.zeros(nb_years) @ np.identity(nb_years)
+        d_energy_wasted_objective_d_damage_frac_output = self.macro_model.grad_energy_wasted_objective(d_sum_energy_wasted_d_damage_frac_output, d_sum_energy_total_d_damage_frac_output)
 
         self.set_partial_derivative_for_other_types(
             (GlossaryCore.EconomicsDfValue, GlossaryCore.GrossOutput),
@@ -414,6 +413,10 @@ class MacroeconomicsDiscipline(ClimateEcoDiscipline):
             (GlossaryCore.EconomicsDfValue, GlossaryCore.EnergyWasted),
             (GlossaryCore.DamageDfValue, GlossaryCore.DamageFractionOutput),
             d_Ew_d_dfo)
+        self.set_partial_derivative_for_other_types(
+            (GlossaryCore.EnergyWastedObjective,),
+            (GlossaryCore.DamageDfValue, GlossaryCore.DamageFractionOutput),
+            d_energy_wasted_objective_d_damage_frac_output)
 
         # Compute gradients wrt population_df
         d_consumption_pc_d_population = self.macro_model.d_consumption_pc_d_population()
@@ -434,6 +437,12 @@ class MacroeconomicsDiscipline(ClimateEcoDiscipline):
             d_net_output_d_work_age_population, d_investment_d_working_age_population)
         d_consumption_pc_d_working_age_population = self.macro_model.d_consumption_per_capita_d_user_input(
             d_consumption_d_working_age_population)
+        # gradient of the energy_wasted_objective
+        d_sum_energy_wasted_d_working_age_population = np.ones(nb_years) @ d_Ew_d_wap
+        d_sum_energy_total_d_working_age_population = np.zeros(nb_years) @ np.identity(nb_years)
+        d_energy_wasted_objective_d_working_age_population = self.macro_model.grad_energy_wasted_objective(
+            d_sum_energy_wasted_d_working_age_population, d_sum_energy_total_d_working_age_population)
+
 
         self.set_partial_derivative_for_other_types(
             (GlossaryCore.WorkforceDfValue, GlossaryCore.Workforce),
@@ -474,6 +483,10 @@ class MacroeconomicsDiscipline(ClimateEcoDiscipline):
             (GlossaryCore.EconomicsDfValue, GlossaryCore.EnergyWasted),
             (GlossaryCore.WorkingAgePopulationDfValue, GlossaryCore.Population1570),
             d_Ew_d_wap)
+        self.set_partial_derivative_for_other_types(
+            (GlossaryCore.EnergyWastedObjective,),
+            (GlossaryCore.WorkingAgePopulationDfValue, GlossaryCore.Population1570),
+            d_energy_wasted_objective_d_working_age_population)
 
 
         # Compute gradients with respect to energy_investment