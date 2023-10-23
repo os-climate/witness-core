da27217c3f2a5e48b57b57daa34200f124e6f8c1
Marie Morere <97039524+sostrades-mariemorere@users.noreply.github.com>
2023-10-23 17:18:27 +0200

    add data for invest sectorization

--- climateeconomics/database_witness_core.py
+++ climateeconomics/database_witness_core.py
@@ -22,4 +22,55 @@ class DatabaseWitnessCore:
                                     link="lol.com",
                                     source="IEA rapport XXXX",
                                     last_update_date=date(2023, 10, 13))
+    #Data for sectorization
+    InvestInduspercofgdp2020 = ColectedData(5.831,
+                                    description="Investment in Industry sector as percentage of GDP for year 2020",
+                                    link="",
+                                    source="Computed from World bank,IMF and IEA data",
+                                    last_update_date=date(2023, 10, 23))
+
+    InvestServicespercofgdp2020 = ColectedData(19.231,
+                                    description="Investment in Services sector as percentage of GDP for year 2020",
+                                    link="",
+                                    source="Computed from World bank and IMF data",
+                                    last_update_date=date(2023, 10, 23))
+
+
+    InvestAgriculturepercofgdp2020 = ColectedData(0.4531,
+                                    description="Investment in Agriculture sector as percentage of GDP for year 2020",
+                                    link="",
+                                    source="Computed from World bank, IMF, and FAO gross capital formation data",
+                                    last_update_date=date(2023, 10, 23))
+
+    EnergyshareAgriculture2020 = ColectedData(2.1360,
+                                                  description="Share of net energy production dedicated to Agriculture sector in % in 2020",
+                                                  link="",
+                                                  source="IEA",
+                                                  last_update_date=date(2023, 10, 23))
+
+    EnergyshareIndustry2020 = ColectedData(28.9442,
+                                              description="Share of net energy production dedicated to Industry sector in % in 2020",
+                                              link="",
+                                              source="IEA",
+                                              last_update_date=date(2023, 10, 23))
+
+    EnergyshareServices2020 = ColectedData(36.9954,
+                                              description="Share of net energy production dedicated to Services sector in % in 2020",
+                                              link="",
+                                              source="IEA",
+                                              last_update_date=date(2023, 10, 23))
+
+    EnergyshareResidential2020 = ColectedData(21.00,
+                                           description="Share of net energy production dedicated to Residential in % in 2020",
+                                           link="",
+                                           source="IEA",
+                                           last_update_date=date(2023, 10, 23))
+
+    EnergyshareOther2020 = ColectedData(10.9230,
+                                           description="Share of net energy production dedicated to other consumption in % in 2020",
+                                           link="",
+                                           source="IEA",
+                                           last_update_date=date(2023, 10, 23))
+
+
 
