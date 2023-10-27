3aca9b1e7e0d1c7858aade70e84babdc7eb67eec
Yasser BEKKALI <yasser.bekkali@capgemini.com>
2023-10-27 11:11:44 +0200

    delete investments in core as it is in energy

--- climateeconomics/database_witness_core.py
+++ climateeconomics/database_witness_core.py
@@ -16,18 +16,6 @@ class ColectedData:
 
 class DatabaseWitnessCore:
     """Stocke les valeurs utilisées dans witness core"""
-    # Example :
-    InvestFossil2020 = ColectedData(839.,
-                                    description="Investment in fossil in 2020 in G US$",
-                                    link="https://www.iea.org/reports/world-energy-investment-2023/overview-and-key-findings",
-                                    source="World energy investment",
-                                    last_update_date=date(2023, 10, 26))
-
-    InvestCleanEnergy2020 = ColectedData(1259.,
-                                    description="Investment in clean energy in 2020 in G US$",
-                                    link="https://www.iea.org/reports/world-energy-investment-2023/overview-and-key-findings",
-                                    source="World energy investment",
-                                    last_update_date=date(2023, 10, 26))
     #Data for sectorization
     InvestInduspercofgdp2020 = ColectedData(5.831,
                                     description="Investment in Industry sector as percentage of GDP for year 2020",
