f61c3b0f086bf0147c64c8d2a4c02fc42ab2111f
ggoyon <guillaume.goyon@capgemini.com>
2023-11-06 13:25:24 +0100

    Fixed headers without Apache

--- climateeconomics/sos_processes/iam/witness/witness_optim_sub_process/usecase_witness_optim_sub.py
+++ climateeconomics/sos_processes/iam/witness/witness_optim_sub_process/usecase_witness_optim_sub.py
@@ -1,6 +1,20 @@
+'''
+Copyright 2022 Airbus SAS
+Modifications on 2023/04/19-2023/10/27 Copyright 2023 Capgemini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 # mode: python; py-indent-offset: 4; tab-width: 8; coding:utf-8
-# Copyright (c) 2021 Airbus SAS.
-# All rights reserved.
 from climateeconomics.glossarycore import GlossaryCore
 from energy_models.core.stream_type.energy_models.biomass_dry import BiomassDry
 from sostrades_core.study_manager.study_manager import StudyManager
