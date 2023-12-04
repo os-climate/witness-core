22291e41f185f54f60e159f294f86de4b02a936e
perrotcap <antoine.perrot@capgemini.com>
2023-12-04 09:16:17 +0100

    completed sectorization process with DemandDiscipline

--- climateeconomics/sos_processes/iam/witness/sectorization_process/process.py
+++ climateeconomics/sos_processes/iam/witness/sectorization_process/process.py
@@ -47,12 +47,11 @@ class ProcessBuilder(BaseProcessBuilder):
         mods_dict = {'Population':'climateeconomics.sos_wrapping.sos_wrapping_witness.population.population_discipline.PopulationDiscipline',
                      'LaborMarket': 'climateeconomics.sos_wrapping.sos_wrapping_sectors.labor_market.labor_market_discipline.LaborMarketDiscipline',
                      'Consumption':'climateeconomics.sos_wrapping.sos_wrapping_witness.consumption.consumption_discipline.ConsumptionDiscipline',
+                     'SectorsDemand':'climateeconomics.sos_wrapping.sos_wrapping_sectors.demand.demand_discipline.DemandDiscipline',
                      'SectorsInvestDistribution': 'climateeconomics.sos_wrapping.sos_wrapping_sectors.sectors_redistribution_invests.sectors_redistribution_invest_discipline.SectorsRedistributionInvestsDiscipline',
                      'SectorsEnergyDistribution': 'climateeconomics.sos_wrapping.sos_wrapping_sectors.sectors_redistribution_energy.sectors_redistribution_energy_discipline.SectorsRedistributionEnergyDiscipline'}
                            
         builder_list = self.create_builder_list(mods_dict, ns_dict=ns_dict)
-
-
 
         chain_builders.append(builder_list)
 
