de9965cb0aee1fb8bfbc65a99c5d31cf25f5b40a
benherry <benjamin.herry@capgemini.com>
2023-12-19 12:11:47 +0100

    [fix]: energy production net for fossil and renewable

--- climateeconomics/sos_wrapping/sos_wrapping_witness/post_proc_witness_ms/post_processing_witness_coarse_mda.py
+++ climateeconomics/sos_wrapping/sos_wrapping_witness/post_proc_witness_ms/post_processing_witness_coarse_mda.py
@@ -398,7 +398,7 @@ def post_processings(execution_engine, namespace, filters):
 
     if 'Total production per scenario' in graphs_list:
 
-        chart_name = 'Total Energy production per scenario'
+        chart_name = 'Total Net Energy production per scenario'
         x_axis_name = 'Years'
         y_axis_name = GlossaryCore.TotalProductionValue + ' [TWh]'
 
@@ -426,7 +426,7 @@ def post_processings(execution_engine, namespace, filters):
         x_axis_name = 'Years'
         y_axis_name = 'Fossil energy production [TWh]'
 
-        df_paths = [f'{EnergyMix.name}.energy_production_brut_detailed']
+        df_paths = [f'{EnergyMix.name}.energy_production_detailed']
         (energy_production_brut_detailed_df_dict,) = get_df_per_scenario_dict(execution_engine, df_paths)
 
         energy_production_brut_detailed_dict = {}
@@ -446,7 +446,7 @@ def post_processings(execution_engine, namespace, filters):
         x_axis_name = 'Years'
         y_axis_name = 'Renewable energy production [TWh]'
 
-        df_paths = [f'{EnergyMix.name}.energy_production_brut_detailed']
+        df_paths = [f'{EnergyMix.name}.energy_production_detailed']
         (energy_production_brut_detailed_df_dict,) = get_df_per_scenario_dict(execution_engine, df_paths)
 
         energy_production_brut_detailed_dict = {}
