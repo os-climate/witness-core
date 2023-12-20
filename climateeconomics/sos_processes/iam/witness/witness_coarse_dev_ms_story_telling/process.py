47e8bbf913ffa5d638bb13a2fcf1d524450ae0d2
benherry <benjamin.herry@capgemini.com>
2023-12-20 08:51:56 +0100

    [fix] header correction

--- climateeconomics/sos_processes/iam/witness/witness_coarse_dev_ms_story_telling/process.py
+++ climateeconomics/sos_processes/iam/witness/witness_coarse_dev_ms_story_telling/process.py
@@ -1,5 +1,6 @@
 '''
 Copyright 2023 Capgemini
+Modifications on 2023/06/14-2023/12/19 Copyright 2023 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
