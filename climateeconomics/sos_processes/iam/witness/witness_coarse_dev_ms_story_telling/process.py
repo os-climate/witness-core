e280889acaadc46e36b50b8e84c071c76b009156
benherry <benjamin.herry@capgemini.com>
2024-01-08 14:39:26 +0100

    [fix]: cleaning mda scenario name

--- climateeconomics/sos_processes/iam/witness/witness_coarse_dev_ms_story_telling/process.py
+++ climateeconomics/sos_processes/iam/witness/witness_coarse_dev_ms_story_telling/process.py
@@ -72,8 +72,9 @@ class ProcessBuilder(BaseProcessBuilder):
 
         # Add new namespaces needed for the scatter multiscenario
         ns_dict = {'ns_scatter_scenario': f'{self.ee.study_name}.{scatter_scenario_name}',
-                   'ns_post_processing': f'{self.ee.study_name}.mda_scenarios',
-                   'ns_ref': f'{self.ee.study_name}.{scatter_scenario_name}.NormalizationReferences'}
+                   'ns_post_processing': f'{self.ee.study_name}.{scatter_scenario_name}',
+                   'ns_ref': f'{self.ee.study_name}.{scatter_scenario_name}.NormalizationReferences',
+                   }
 
 
         self.ee.ns_manager.add_ns_def(ns_dict)
@@ -81,7 +82,7 @@ class ProcessBuilder(BaseProcessBuilder):
                                                                                   'ns_scatter_scenario']})
 
         multi_scenario = self.ee.factory.create_driver(
-            'mda_scenarios', builder_cdf_list, flatten_subprocess=False, map_name='new_map'
+            scatter_scenario_name, builder_cdf_list, flatten_subprocess=False, map_name='new_map'
         )
         self.ee.post_processing_manager.add_post_processing_module_to_namespace('ns_post_processing',
                                                                                 'climateeconomics.sos_wrapping.sos_wrapping_witness.post_proc_witness_ms.post_processing_witness_coarse_mda')
