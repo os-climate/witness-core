[fix] review normalization of CO2 emissions objective function.

climateeconomics/core/core_emissions/ghg_emissions_model.py
@@ -126,16 +126,16 @@ class GHGEmissions():
         When it reaches - CO2emissionsRef, then the energy sector is net zero emission and objective function should be 0
         When CO2 emissions are max, in full fossil, mean emissions between 2020 and 2100 are around 8331 Gt
         For the full fossil case,  CO2emissionsRef + cumulated(CO2_emissions between 2020 and 2100 =  1772 + 8331 = 10103
-        to keep the objective function between 0 and 1, it is sufficient to normalize the sum above by 1.5 * CO2emiisionsRef
+        to keep the objective function between 0 and 1, it is sufficient to normalize the sum above by 10 * CO2emiisionsRef
         '''
         self.co2_emissions_objective = (self.CO2EmissionsRef + self.GHG_total_energy_emissions[GlossaryCore.TotalCO2Emissions].sum()) / \
-                                       (1.5 * self.CO2EmissionsRef)
+                                       (10. * self.CO2EmissionsRef)
 
     def d_CO2_emissions_objective_d_total_co2_emissions(self):
         '''
         Compute gradient of CO2 emissions objective wrt ToTalCO2Emissions
         '''
-        d_CO2_emissions_objective_d_total_co2_emissions = np.ones(len(self.years_range)) / (1.5 * self.CO2EmissionsRef)
+        d_CO2_emissions_objective_d_total_co2_emissions = np.ones(len(self.years_range)) / (10. * self.CO2EmissionsRef)
 
         return d_CO2_emissions_objective_d_total_co2_emissions
 

climateeconomics/tests/l0_test_ghgemissions_discipline.py
@@ -71,7 +71,7 @@ class GHGEmissionDiscTest(unittest.TestCase):
 
         CO2_indus_emissions_df = pd.DataFrame({GlossaryCore.Years: years,
                                                'indus_emissions': np.linspace(1., 2., len(years))})
-        CO2_emissions_ref = 1047.96 # Gt
+        CO2_emissions_ref = 1772.8 # Gt
 
         values_dict = {f'{self.name}.{GlossaryCore.YearStart}': year_start,
                        f'{self.name}.{GlossaryCore.YearEnd}': year_end,