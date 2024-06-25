96699fee1a5cf56d4f5f3f1e59272e36ab507d5e
perrotcap <antoine.perrot@capgemini.com>
2024-06-25 08:58:23 +0200

    fixed headers

--- climateeconomics/tests/l1s_test_all_usecases.py
+++ climateeconomics/tests/l1s_test_all_usecases.py
@@ -1,5 +1,6 @@
 '''
-Copyright 2024 Capgemini
+Copyright 2022 Airbus SAS
+Modifications on {} Copyright 2024 Capgemini
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -13,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 '''
+
 
 import pprint
 import unittest
