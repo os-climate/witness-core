e989d57ef2a66636f58c106e97284fd4389f8947
Simão Rodrigues <simao.santos-rodrigues@capgemini.com>
2024-08-01 10:11:27 +0200

    refactor: rename demand_discipline.py to consumption_discipline.py

--- climateeconomics/sos_processes/iam/witness/sectorization_process/process.py
+++ climateeconomics/sos_processes/iam/witness/sectorization_process/process.py
@@ -54,7 +54,7 @@ class ProcessBuilder(BaseProcessBuilder):
         mods_dict = {'Population':'climateeconomics.sos_wrapping.sos_wrapping_witness.population.population_discipline.PopulationDiscipline',
                      'LaborMarket': 'climateeconomics.sos_wrapping.sos_wrapping_sectors.labor_market.labor_market_discipline.LaborMarketDiscipline',
                      'Consumption':'climateeconomics.sos_wrapping.sos_wrapping_witness.consumption.consumption_discipline.ConsumptionDiscipline',
-                     'SectorsDemand':'climateeconomics.sos_wrapping.sos_wrapping_sectors.demand.demand_discipline.ConsumptionDiscipline',
+                     'SectorsDemand':'climateeconomics.sos_wrapping.sos_wrapping_sectors.demand.consumption_discipline.ConsumptionDiscipline',
                      'SectorsInvestDistribution': 'climateeconomics.sos_wrapping.sos_wrapping_sectors.sectors_redistribution_invests.sectors_redistribution_invest_discipline.SectorsRedistributionInvestsDiscipline',
                      'SectorsEnergyDistribution': 'climateeconomics.sos_wrapping.sos_wrapping_sectors.sectors_redistribution_energy.sectors_redistribution_energy_discipline.SectorsRedistributionEnergyDiscipline',
                      GHGemissionsDiscipline.name: 'climateeconomics.sos_wrapping.sos_wrapping_emissions.ghgemissions.ghgemissions_discipline.GHGemissionsDiscipline'}
