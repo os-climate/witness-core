be2c8d93ef6fd9b2770a4529d97e8ef5a1865088
Simão Rodrigues <simao.santos-rodrigues@capgemini.com>
2024-08-01 10:24:56 +0200

    ruff format consumption_discipline.py

--- climateeconomics/sos_wrapping/sos_wrapping_sectors/demand/consumption_discipline.py
+++ climateeconomics/sos_wrapping/sos_wrapping_sectors/demand/consumption_discipline.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright 2023 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-'''
+"""
+
 import numpy as np
 from sostrades_core.execution_engine.sos_wrapp import SoSWrapp
 from sostrades_core.tools.post_processing.charts.chart_filter import ChartFilter
@@ -29,29 +30,28 @@ from climateeconomics.sos_wrapping.sos_wrapping_sectors.demand.demand_model impo
 
 class ConsumptionDiscipline(SoSWrapp):
     """Discipline demand"""
+
     # ontology information
     _ontology_data = {
-        'label': 'Demand WITNESS Model',
-        'type': 'Research',
-        'source': 'SoSTrades Project',
-        'validated': '',
-        'validated_by': 'SoSTrades Project',
-        'last_modification_date': '',
-        'category': '',
-        'definition': '',
-        'icon': 'fa-solid fa-chart-pie',
-        'version': '',
+        "label": "Demand WITNESS Model",
+        "type": "Research",
+        "source": "SoSTrades Project",
+        "validated": "",
+        "validated_by": "SoSTrades Project",
+        "last_modification_date": "",
+        "category": "",
+        "definition": "",
+        "icon": "fa-solid fa-chart-pie",
+        "version": "",
     }
-    _maturity = 'Research'
+    _maturity = "Research"
 
     DESC_IN = {
         GlossaryCore.SectorListValue: GlossaryCore.SectorList,
         GlossaryCore.PopulationDfValue: GlossaryCore.PopulationDf,
     }
 
-    DESC_OUT = {
-        GlossaryCore.AllSectorsDemandDfValue: GlossaryCore.AllSectorsDemandDf
-    }
+    DESC_OUT = {GlossaryCore.AllSectorsDemandDfValue: GlossaryCore.AllSectorsDemandDf}
 
     def setup_sos_disciplines(self):
         """setup dynamic inputs and outputs"""
@@ -61,8 +61,22 @@ class ConsumptionDiscipline(SoSWrapp):
         if GlossaryCore.SectorListValue in self.get_data_in():
             sector_list = self.get_sosdisc_inputs(GlossaryCore.SectorListValue)
             for sector in sector_list:
-                dynamic_inputs[f'{sector}.{GlossaryCore.SectorDemandPerCapitaDfValue}'] = GlossaryCore.get_dynamic_variable(GlossaryCore.SectorDemandPerCapitaDf)
-                dynamic_outputs[f'{sector}.{GlossaryCore.SectorGDPDemandDfValue}'] = GlossaryCore.get_dynamic_variable(GlossaryCore.SectorGDPDemandDf)
+                dynamic_inputs[
+                    f"{sector}.{GlossaryCore.SectorDemandPerCapitaDfValue}"
+                ] = GlossaryCore.get_dynamic_variable(
+                    GlossaryCore.SectorDemandPerCapitaDf
+                )
+
+                dynamic_inputs[f"{sector}.{GlossaryCore.InvestmentDfValue}"] = (
+                    GlossaryCore.get_dynamic_variable(GlossaryCore.InvestmentDf)
+                )
+                dynamic_inputs[f"{sector}.{GlossaryCore.ProductionDfValue}"] = (
+                    GlossaryCore.get_dynamic_variable(GlossaryCore.ProductionDf)
+                )
+
+                dynamic_outputs[f"{sector}.{GlossaryCore.SectorGDPDemandDfValue}"] = (
+                    GlossaryCore.get_dynamic_variable(GlossaryCore.SectorGDPDemandDf)
+                )
 
         self.add_inputs(dynamic_inputs)
         self.add_outputs(dynamic_outputs)
@@ -82,7 +96,9 @@ class ConsumptionDiscipline(SoSWrapp):
         }
 
         for sector in sector_list:
-            outputs[f'{sector}.{GlossaryCore.SectorGDPDemandDfValue}'] = sectors_demand[sector]
+            outputs[f"{sector}.{GlossaryCore.SectorGDPDemandDfValue}"] = sectors_demand[
+                sector
+            ]
 
         self.store_sos_outputs_values(outputs)
 
@@ -91,30 +107,46 @@ class ConsumptionDiscipline(SoSWrapp):
         inputs = self.get_sosdisc_inputs()
 
         sectors_list = inputs[GlossaryCore.SectorListValue]
-        population = inputs[GlossaryCore.PopulationDfValue][GlossaryCore.PopulationValue].values
+        population = inputs[GlossaryCore.PopulationDfValue][
+            GlossaryCore.PopulationValue
+        ].values
 
         for sector in sectors_list:
-            sector_demand_per_capita = inputs[f'{sector}.{GlossaryCore.SectorDemandPerCapitaDfValue}'][GlossaryCore.SectorDemandPerCapitaDfValue].values
+            sector_demand_per_capita = inputs[
+                f"{sector}.{GlossaryCore.SectorDemandPerCapitaDfValue}"
+            ][GlossaryCore.SectorDemandPerCapitaDfValue].values
             self.set_partial_derivative_for_other_types(
-                (f'{sector}.{GlossaryCore.SectorGDPDemandDfValue}', GlossaryCore.SectorGDPDemandDfValue),
+                (
+                    f"{sector}.{GlossaryCore.SectorGDPDemandDfValue}",
+                    GlossaryCore.SectorGDPDemandDfValue,
+                ),
                 (GlossaryCore.PopulationDfValue, GlossaryCore.PopulationValue),
-                np.diag(sector_demand_per_capita * 1e-6)
+                np.diag(sector_demand_per_capita * 1e-6),
             )
 
             self.set_partial_derivative_for_other_types(
-                (f'{sector}.{GlossaryCore.SectorGDPDemandDfValue}', GlossaryCore.SectorGDPDemandDfValue),
-                (f'{sector}.{GlossaryCore.SectorDemandPerCapitaDfValue}', GlossaryCore.SectorDemandPerCapitaDfValue),
-                np.diag(population) * 1e-6
+                (
+                    f"{sector}.{GlossaryCore.SectorGDPDemandDfValue}",
+                    GlossaryCore.SectorGDPDemandDfValue,
+                ),
+                (
+                    f"{sector}.{GlossaryCore.SectorDemandPerCapitaDfValue}",
+                    GlossaryCore.SectorDemandPerCapitaDfValue,
+                ),
+                np.diag(population) * 1e-6,
             )
 
     def get_chart_filter_list(self):
         chart_filters = []
 
-        chart_list = [GlossaryCore.SectorGDPDemandDf,
-                      GlossaryCore.SectorDemandPerCapitaDfValue,]
+        chart_list = [
+            GlossaryCore.SectorGDPDemandDf,
+            GlossaryCore.SectorDemandPerCapitaDfValue,
+        ]
 
-        chart_filters.append(ChartFilter(
-            'Charts filter', chart_list, chart_list, 'charts'))
+        chart_filters.append(
+            ChartFilter("Charts filter", chart_list, chart_list, "charts")
+        )
 
         return chart_filters
 
@@ -130,15 +162,20 @@ class ConsumptionDiscipline(SoSWrapp):
         if all_filters or GlossaryCore.SectorGDPDemandDf:
             # first graph
             all_sectors_demand_df = self.get_sosdisc_outputs(
-                GlossaryCore.AllSectorsDemandDfValue)
+                GlossaryCore.AllSectorsDemandDfValue
+            )
             sector_list = self.get_sosdisc_inputs(GlossaryCore.SectorListValue)
 
-            chart_name = f"Demand by sectors [{GlossaryCore.AllSectorsDemandDf['unit']}]"
+            chart_name = (
+                f"Demand by sectors [{GlossaryCore.AllSectorsDemandDf['unit']}]"
+            )
 
-            new_chart = TwoAxesInstanciatedChart(GlossaryCore.Years,
-                                                 GlossaryCore.AllSectorsDemandDf['unit'],
-                                                 stacked_bar=True,
-                                                 chart_name=chart_name)
+            new_chart = TwoAxesInstanciatedChart(
+                GlossaryCore.Years,
+                GlossaryCore.AllSectorsDemandDf["unit"],
+                stacked_bar=True,
+                chart_name=chart_name,
+            )
 
             years = list(all_sectors_demand_df[GlossaryCore.Years])
 
@@ -146,31 +183,30 @@ class ConsumptionDiscipline(SoSWrapp):
             for sector in sector_list:
                 demand_sector_per_capita = all_sectors_demand_df[sector].values
                 total_demand.append(demand_sector_per_capita)
-                new_series = InstanciatedSeries(years,
-                                                list(demand_sector_per_capita),
-                                                sector, 'bar', True)
+                new_series = InstanciatedSeries(
+                    years, list(demand_sector_per_capita), sector, "bar", True
+                )
                 new_chart.series.append(new_series)
             total_demand = np.sum(total_demand, axis=0)
-            new_series = InstanciatedSeries(years,
-                                            list(total_demand),
-                                            'Total', 'lines', True)
+            new_series = InstanciatedSeries(
+                years, list(total_demand), "Total", "lines", True
+            )
             new_chart.series.append(new_series)
             instanciated_charts.append(new_chart)
 
             # second graph
             chart_name = "Share of demand by sector [%]"
 
-            new_chart = TwoAxesInstanciatedChart(GlossaryCore.Years,
-                                                 '%',
-                                                 stacked_bar=True,
-                                                 chart_name=chart_name)
+            new_chart = TwoAxesInstanciatedChart(
+                GlossaryCore.Years, "%", stacked_bar=True, chart_name=chart_name
+            )
 
             for sector in sector_list:
                 demand_sector_per_capita = all_sectors_demand_df[sector].values
-                share_sector = demand_sector_per_capita / total_demand * 100.
-                new_series = InstanciatedSeries(years,
-                                                list(share_sector),
-                                                sector, 'bar', True)
+                share_sector = demand_sector_per_capita / total_demand * 100.0
+                new_series = InstanciatedSeries(
+                    years, list(share_sector), sector, "bar", True
+                )
                 new_chart.series.append(new_series)
 
             instanciated_charts.append(new_chart)
@@ -180,20 +216,25 @@ class ConsumptionDiscipline(SoSWrapp):
 
             chart_name = f"Demand per person by sectors [{GlossaryCore.SectorDemandPerCapitaDf['unit']}]"
 
-            new_chart = TwoAxesInstanciatedChart(GlossaryCore.Years,
-                                                 GlossaryCore.SectorDemandPerCapitaDf['unit'],
-                                                 stacked_bar=False,
-                                                 chart_name=chart_name)
+            new_chart = TwoAxesInstanciatedChart(
+                GlossaryCore.Years,
+                GlossaryCore.SectorDemandPerCapitaDf["unit"],
+                stacked_bar=False,
+                chart_name=chart_name,
+            )
 
             for sector in sector_list:
-                demand_sector_per_capita_df = self.get_sosdisc_inputs(f'{sector}.{GlossaryCore.SectorDemandPerCapitaDfValue}')
+                demand_sector_per_capita_df = self.get_sosdisc_inputs(
+                    f"{sector}.{GlossaryCore.SectorDemandPerCapitaDfValue}"
+                )
                 years = list(demand_sector_per_capita_df[GlossaryCore.Years].values)
-                demand_sector_per_capita = demand_sector_per_capita_df[GlossaryCore.SectorDemandPerCapitaDfValue].values
-                new_series = InstanciatedSeries(years,
-                                                list(demand_sector_per_capita),
-                                                sector, 'lines', True)
+                demand_sector_per_capita = demand_sector_per_capita_df[
+                    GlossaryCore.SectorDemandPerCapitaDfValue
+                ].values
+                new_series = InstanciatedSeries(
+                    years, list(demand_sector_per_capita), sector, "lines", True
+                )
                 new_chart.series.append(new_series)
 
             instanciated_charts.append(new_chart)
         return instanciated_charts
-
