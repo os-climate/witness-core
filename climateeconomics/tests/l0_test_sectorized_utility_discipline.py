1efe52c46e15f110e001d8cdd1bcbddc1acd38d7
Simão Rodrigues <simao.santos-rodrigues@capgemini.com>
2024-09-11 10:04:14 +0200

    fix: headers

--- climateeconomics/tests/l0_test_sectorized_utility_discipline.py
+++ climateeconomics/tests/l0_test_sectorized_utility_discipline.py
@@ -1,6 +1,5 @@
 '''
-Copyright 2022 Airbus SAS
-Modifications on 2023/08/17-2023/11/03 Copyright 2023 Capgemini
+Copyright 2024 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
+
 import unittest
 from os.path import dirname, join
 
