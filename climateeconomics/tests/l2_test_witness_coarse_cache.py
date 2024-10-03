c7f5bdf16da8d3a52cfbcc052f931ecff7e15125
Valentin Joncquieres <valentin.joncquieres@capgemini.com>
2024-10-03 15:20:33 +0200

    fix ruff

--- climateeconomics/tests/l2_test_witness_coarse_cache.py
+++ climateeconomics/tests/l2_test_witness_coarse_cache.py
@@ -19,8 +19,8 @@ import unittest
 
 from energy_models.core.energy_process_builder import INVEST_DISCIPLINE_OPTIONS
 from energy_models.glossaryenergy import GlossaryEnergy
-from sostrades_core.tools.compare_data_manager_tooling import compare_dict
 from sostrades_core.execution_engine.execution_engine import ExecutionEngine
+from sostrades_core.tools.compare_data_manager_tooling import compare_dict
 
 from climateeconomics.sos_processes.iam.witness.witness_coarse_optim_process.usecase_witness_optim_invest_distrib import (
     Study as witness_proc_usecase,
