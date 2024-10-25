135a0879dfb9a64cbe654c5c8d763e07bcbac18d
perrotcap <antoine.perrot@capgemini.com>
2024-10-25 11:56:31 +0200

    improved post proc IAMC

--- climateeconomics/sos_processes/iam/witness/witness_coarse_dev_ms_optim_process/usecase_ms_2_tipping_point_2023.py
+++ climateeconomics/sos_processes/iam/witness/witness_coarse_dev_ms_optim_process/usecase_ms_2_tipping_point_2023.py
@@ -95,10 +95,14 @@ class Study(ClimateEconomicsStudyManager):
         })
 
         values_dict = self.update_dataframes_with_year_star(values_dict=values_dict, year_start=self.year_start)
+        values_dict.update(
+            {f"{self.study_name}.{scatter_scenario}.{scenario_name}.WITNESS_MDO.max_iter": 2 for scenario_name in
+             scenario_dict.keys()})
 
         return values_dict
 
 
 if '__main__' == __name__:
     uc_cls = Study(run_usecase=True)
+    uc_cls.load_data()
     uc_cls.test()
