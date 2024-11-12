8b2469ce446075fd7bbafbbb2743a874106d61b9
perrotcap <antoine.perrot@capgemini.com>
2024-11-12 16:15:16 +0100

    fixed headers

--- climateeconomics/calibration/crop/data.py
+++ climateeconomics/calibration/crop/data.py
@@ -1,4 +1,4 @@
-"""
+'''
 Copyright 2024 Capgemini
 Modifications on 2023/04/19-2024/06/24 Copyright 2023 Capgemini
 
@@ -13,7 +13,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-"""
+'''
 from climateeconomics.calibration.crop.tools import (
     CalibrationData,
     solve_share_prod_waste,
