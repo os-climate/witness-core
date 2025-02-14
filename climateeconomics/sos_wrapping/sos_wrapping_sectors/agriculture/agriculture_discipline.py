6bced891a74aa9a4d564dc7e418f2612ddaeb831
perrotcap <antoine.perrot@capgemini.com>
2025-02-14 10:32:32 +0100

    fixed header

--- climateeconomics/sos_wrapping/sos_wrapping_sectors/agriculture/agriculture_discipline.py
+++ climateeconomics/sos_wrapping/sos_wrapping_sectors/agriculture/agriculture_discipline.py
@@ -1,6 +1,6 @@
 '''
-Copyright 2024 Capgemini
-Modifications on 2023/06/14-2023/11/03 Copyright 2023 Capgemini
+Copyright 2022 Airbus SAS
+Modifications on 2023/06/14-2023/11/03 Copyright 2025 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
